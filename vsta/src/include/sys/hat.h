0102452bc0790ab33ee551c09222f1e81ea9d058
vandys <vandys>
1993-02-02 15:13:28 +0000

    Add hat_fork() entry.  Make procedure declarations extern.

--- vsta/src/include/sys/hat.h
+++ vsta/src/include/sys/hat.h
@@ -15,36 +15,42 @@
  *
  * Returns 0 if a translation does not exist
  */
-void *hat_vtop(struct vas *, void *);
+extern void *hat_vtop(struct vas *, void *);
 
 /*
  * hat_initvas()
  *	Initialize a vas
  */
-void hat_initvas(struct vas *);
+extern void hat_initvas(struct vas *);
 
 /*
  * hat_addtrans()
  *	Add a translation
  */
-void hat_addtrans(struct pview *pv, void *vaddr, uint pfn, int write);
+extern void hat_addtrans(struct pview *pv, void *vaddr, uint pfn, int write);
 
 /*
  * hat_deletetrans()
  *	Delete a translation
  */
-void hat_deletetrans(struct pview *pv, void *vaddr, uint pfn);
+extern void hat_deletetrans(struct pview *pv, void *vaddr, uint pfn);
 
 /*
  * hat_attach()
  *	Try to attach a new view
  */
-int hat_attach(struct pview *pv, void *vaddr);
+extern int hat_attach(struct pview *pv, void *vaddr);
 
 /*
  * hat_detach()
  *	Detach existing view
  */
-void hat_detach(struct pview *pv);
+extern void hat_detach(struct pview *pv);
+
+/*
+ * hat_fork()
+ *	Hook for handling details of fork()'ing a vas
+ */
+extern void hat_fork(struct vas *, struct vas *);
 
 #endif /* _HAT_H */
