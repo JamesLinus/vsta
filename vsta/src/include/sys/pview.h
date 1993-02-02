49967b99c07841a55b0b2f47a9fbd7bd26114aa7
vandys <vandys>
1993-02-02 15:14:44 +0000

    Add flag to indicate pview is in fork().  Needed to flag this to hat, which otherwise might take exception to some of the addresses provided in the p_vaddr field.

--- vsta/src/include/sys/pview.h
+++ vsta/src/include/sys/pview.h
@@ -25,6 +25,7 @@ struct pview {
 #define PROT_RO (1)		/* Read only */
 #define PROT_KERN (2)		/* Kernel only */
 #define PROT_MMAP (4)		/* Created by mmap() */
+#define PROT_FORK (8)		/* View is in process of fork() */
 
 #ifdef KERNEL
 /*
