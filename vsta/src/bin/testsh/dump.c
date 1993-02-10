6df458bd285a951b0ec2e28b3bf1648774493886
vandys <vandys>
1993-02-10 18:11:56 +0000

    Initial revision

---
+++ vsta/src/bin/testsh/dump.c
@@ -0,0 +1,76 @@
+/*
+ * dump.c
+ *	Dump memory in various formats
+ */
+#include <stdio.h>
+
+/*
+ * prpad()
+ *	Print hex number with leading 0 padding
+ */
+static void
+prpad(unsigned long n, int len)
+{
+	char buf[16], *p;
+	int x;
+
+	p = buf+16;
+	*--p = '\0';
+	while (len-- > 0) {
+		x = n & 0xF;
+		n >>= 4;
+		if (x < 10) {
+			*--p = x + '0';
+		} else {
+			*--p = (x-10) + 'a';
+		}
+	}
+	*--p = ' ';
+	printf(p);
+}
+
+/*
+ * dump_s()
+ *	Dump strings
+ */
+void
+dump_s(char *buf, uint count)
+{
+	int x, col = 0;
+
+	for (x = 0; x < count; ++x) {
+		prpad(buf[x] & 0xFF, 2);
+		if (++col >= 16) {
+			int y;
+			char c;
+
+			printf(" ");
+			for (y = 0; y < 16; ++y) {
+				c = buf[x-15+y];
+				if ((c < ' ') || (c >= 0x7F)) {
+					c = '.';
+				}
+				putchar(c);
+			}
+			printf("\n");
+			col = 0;
+		}
+	}
+	if (col) {		/* Partial line */
+		int y;
+
+		for (y = col; y < 16; ++y) {
+			printf("   ");
+		}
+		for (y = 0; y < col; ++y) {
+			char c;
+
+			c = buf[count-col+y];
+			if ((c < ' ') || (c >= 0x7F)) {
+				c = '.';
+			}
+			putchar(c);
+		}
+		printf("\n");
+	}
+}
---
+++ vsta/src/os/mach/dbgio.c
@@ -0,0 +1,44 @@
+/*
+ * dbgio.c
+ *	I/O port access
+ */
+#if defined(DEBUG) || defined(KDB)
+extern char *strchr();
+
+/*
+ * dbg_inport()
+ *	Input a byte from a port
+ */
+void
+dbg_inport(char *p)
+{
+	int x, y;
+
+	x = get_num(p);
+	y = inportb(x);
+	printf("0x%x -> 0x%x\n", x, y);
+}
+
+/*
+ * dbg_outport()
+ *	Output a byte to a port
+ */
+void
+dbg_outport(char *p)
+{
+	char *val;
+	int x, y;
+
+	val = strchr(p, ' ');
+	if (!val) {
+		printf("Usage: outport <port> <value>\n");
+		return;
+	}
+	*val++ = '\0';
+	x = get_num(p);
+	y = get_num(val);
+	outportb(x, y);
+	printf("0x%x <- 0x%x\n", x, y);
+}
+
+#endif /* DEBUG|KDB */
