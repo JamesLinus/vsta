9967ba2190ff42c84ff8532034bb8efbeaef6e8a
vandys <vandys>
1993-02-12 19:44:43 +0000

    Add defs for slave PIC

--- vsta/src/include/mach/isr.h
+++ vsta/src/include/mach/isr.h
@@ -5,5 +5,7 @@
  *	Constants relating to the ISA interrupt architecture
  */
 #define MAX_IRQ 16	/* Max different IRQ levels */
+#define SLAVE_IRQ 8	/* 0..7 are master, 8..15 are slave */
+#define MASTER_SLAVE 2	/* IRQ where slave hooks to master */
 
 #endif /* _MACHISR_H */
