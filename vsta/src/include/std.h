d390bb76f59998878bc39f2ca839ee7ce21736e8
vandys <vandys>
1993-02-18 20:25:55 +0000

    Add bcopy/bcmp prototypes

--- vsta/src/include/std.h
+++ vsta/src/include/std.h
@@ -17,5 +17,7 @@ extern void free(void *);
 extern char *strdup(char *), *strchr(char *, char), *strrchr(char *, char),
 	*index(char *, char), *rindex(char *, char), *strerror(void);
 extern int fork(void), tfork(__voidfun);
+extern int bcopy(void *, void *, unsigned int),
+	bcmp(void *, void *, unsigned int);
 
 #endif /* _STD_H */
