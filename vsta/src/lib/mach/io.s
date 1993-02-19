a9e83bbef49cb95c7dbd5c5a9b8b209987ab79c5
vandys <vandys>
1993-02-19 15:35:11 +0000

    Add block in/out support for disk driver

--- vsta/src/lib/mach/io.s
+++ vsta/src/lib/mach/io.s
@@ -24,3 +24,34 @@ _outportb:
 	movl	8(%esp),%eax
 	outb	%al,%dx
 	ret
+
+/*
+ * repinsw(port, buffer, count)
+ *	Read a bunch of words from an I/O port
+ */
+	.globl	_repinsw
+_repinsw:
+	pushl	%edi
+	movl	0x8(%esp),%edx
+	movl	0xC(%esp),%edi
+	movl	0x10(%esp),%ecx
+	rep
+	insw
+	popl	%edi
+	ret
+
+/*
+ * repoutsw(port, buffer, count)
+ *	Write a bunch of words to an I/O port
+ */
+	.globl	_repoutsw
+_repoutsw:
+	pushl	%esi
+	movl	0x8(%esp),%edx
+	movl	0xC(%esp),%esi
+	movl	0x10(%esp),%ecx
+	rep
+	outsw
+	popl	%esi
+	ret
+
