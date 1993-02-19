75ea312064b9809dc4a3270a906e4f62a86ea778
vandys <vandys>
1993-02-19 15:35:46 +0000

    Add micro and millisecond support

--- vsta/src/lib/time.c
+++ vsta/src/lib/time.c
@@ -17,3 +17,30 @@ sleep(uint secs)
 	time_sleep(&t);
 	return(0);
 }
+
+/*
+ * usleep()
+ *	Like sleep, but in microseconds
+ */
+__usleep(uint usecs)
+{
+	struct time t;
+
+	time_get(&t);
+	t.t_usec += usecs;
+	while (t.t_usec > 1000000) {
+		t.t_sec += 1;
+		t.t_usec -= 1000000;
+	}
+	time_sleep(&t);
+	return(0);
+}
+
+/*
+ * msleep()
+ *	Like sleep, but milliseconds
+ */
+__msleep(uint msecs)
+{
+	return(__usleep(msecs * 1000));
+}
