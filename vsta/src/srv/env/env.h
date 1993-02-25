bf4b01f12ce852b586707217d92bcf4235f1b35e
vandys <vandys>
1993-02-25 17:03:23 +0000

    Add prototypes plus rework environment inheritance

--- vsta/src/srv/env/env.h
+++ vsta/src/srv/env/env.h
@@ -10,7 +10,7 @@
  * of processes.  A name is looked up by starting at the lowest node
  * and searching upward until it is found.
  */
-#include <sys/types.h>
+#include <sys/msg.h>
 #include <sys/perm.h>
 #include <sys/param.h>
 #include <lib/llist.h>
@@ -19,12 +19,15 @@
  * An open file
  */
 struct file {
-	struct node *f_node;	/* Current node we address */
+	struct node *f_node,	/* Current node we address */
+		*f_home;	/* "home" node */
 	struct perm		/* Our abilities */
 		f_perms[PROCPERMS];
 	int f_nperm;		/*  ...# active */
 	off_t f_pos;		/* Position in internal node */
 	int f_mode;		/* Abilities WRT current node */
+	struct file		/* Members sharing f_home */
+		*f_forw,*f_back;
 };
 
 /*
@@ -41,7 +44,7 @@ struct string {
 struct node {
 	struct prot n_prot;	/* Protection of this node */
 	char n_name[NAMESZ];	/* Name of node */
-	ushort n_internal;	/* Internal node? */
+	ushort n_flags;		/* Flags */
 	struct string *n_val;	/*  ...if not, string value for name */
 	struct llist n_elems;	/* Linked list of elements under node */
 	uint n_refs;		/* # references to this node */
@@ -50,10 +53,32 @@ struct node {
 };
 
 /*
+ * Flag bits
+ */
+#define N_INTERNAL 1		/* Is a "directory" */
+
+/*
+ * An oft-asked question
+ */
+#define DIR(n) ((n)->n_flags & N_INTERNAL)
+
+/*
  * Some function prototypes
  */
 extern void deref_val(struct string *),
 	ref_val(struct string *);
 extern struct string *alloc_val(char *);
+extern struct node *node_cow(struct node *);
+extern void env_open(struct msg *, struct file *),
+	env_read(struct msg *, struct file *, uint),
+	env_write(struct msg *, struct file *, uint),
+	env_remove(struct msg *, struct file *),
+	env_stat(struct msg *, struct file *),
+	env_wstat(struct msg *, struct file *),
+	env_seek(struct msg *, struct file *);
+extern struct node *alloc_node(struct file *),
+	*clone_node(struct node *);
+extern void ref_node(struct node *), deref_node(struct node *),
+	remove_node(struct node *);
 
 #endif /* _ENV_H */
