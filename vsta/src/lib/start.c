2ca0ba99e882401c67a2f5725351164278e79470
vandys <vandys>
1993-02-25 21:22:27 +0000

    Add code to restore state from exec()--fdl and command line

--- vsta/src/lib/start.c
+++ vsta/src/lib/start.c
@@ -2,40 +2,63 @@
  * __start()
  *	Do the VSTa-specific stuff to set up argc/argv
  */
-#include <sys/vm.h>
-#include <lib/alloc.h>
+#include <sys/types.h>
+#include <std.h>
+#include <fdl.h>
 
 /*
  * __start()
  *	Do some massaging of arguments during C startup
  */
 void
-__start(int *argcp, char ***argvp)
+__start(int *argcp, char ***argvp, char *a)
 {
-#ifdef LATER
 	char *p, **argv = 0;
-	int argc = 0;
+	int x, argc = 0, len;
 
 	/*
-	 * Walk the argument area.  Each argument is terminated with
-	 * a null byte; two nulls in a row marks the end of all
-	 * arguments.
+	 * Boot programs get this
 	 */
-	for (p = VADDR_ARGS; *p; ++p) {
-		argc += 1;
-		argv = realloc(argv, sizeof(char *)*argc);
-		argv[argc-1] = p;
-		while (*p) {
-			++p;
-		}
+	if (a == 0) {
+noargs:
+		*argcp = 0;
+		*argvp = 0;
+		return;
 	}
+
+	/*
+	 * Get count of arguments
+	 */
+	argc = *(ulong *)a;
+	a += sizeof(ulong);
+	argv = malloc((argc+1) * sizeof(char *));
+	if (argv == 0) {
+		goto noargs;
+	}
+
+	/*
+	 * Walk the argument area.  Copy each string, as we wish
+	 * to tear down the shared memory once we're finished.
+	 */
+	for (x = 0; x < argc; ++x) {
+		len = strlen(a)+1;
+		argv[x] = malloc(len);
+		if (!argv[x]) {
+			goto noargs;
+		}
+		bcopy(a, argv[x], len);
+		a += len;
+	}
+	argv[x] = 0;	/* Traditional */
 
 	/*
 	 * Stuff our values back to our caller
 	 */
 	*argcp = argc;
 	*argvp = argv;
-#else
-	return;
-#endif
+
+	/*
+	 * Restore our fdl state
+	 */
+	__fdl_restore(a);
 }
