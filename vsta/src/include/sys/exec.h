54504dae9805db75dec9b7b699d40609f03a2b2b
vandys <vandys>
1993-02-25 21:20:21 +0000

    Add some commentary about how we pass state through an exec()

--- vsta/src/include/sys/exec.h
+++ vsta/src/include/sys/exec.h
@@ -34,4 +34,40 @@ struct mapfile {
 	struct mapseg m_map[NMAP];	/* The mappings requested */
 };
 
+#ifndef KERNEL
+/*
+ * The exec() system call
+ */
+extern int exec(port_t, struct mapfile *, void *);
+#endif
+
+/*
+ * The third argument to exec() is defined by the C library.  It is
+ * documented here even though the kernel knows nothing about it.
+ * Sue me.
+ *
+ * Three things need to be passed from the old instance of the
+ * process to the new.  First, a command line, which is mapped into
+ * the argc/argv format on the receiving end.  Second, the state of
+ * the file descriptor layer.  Finally, the state of the mount table.
+ * All data is passed in an mmap()'ed sharable region; all data is
+ * packed together end-to-end.
+ *
+ * The command lines is passed as an unsigned long count, followed by
+ * that many null-terminated strings.
+ *
+ * The file descriptor layer is passed as an unsigned long count of
+ * the number file descriptors, followed by that many pairs of
+ * <int, port_t>'s, indicating the association of a file descriptor
+ * to a port.  Port state is then initialized as if this was the first
+ * open on the file descriptor.  XXX it would be very ugly to pass
+ * full state; would it be worth it?
+ *
+ * The mount table is passed as an unsigned long count of the number
+ * of mount table slots.  Each slot is represented by an unsigned
+ * long count of the number of entries under the slot.  Each entry
+ * is a port_t, corresponding to an open connection to a filesystem
+ * server.
+ */
+
 #endif /* _EXEC_H */
