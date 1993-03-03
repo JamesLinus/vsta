3052ba1922b11c713e6cdf18a1e4cd2a9a9de8d6
vandys <vandys>
1993-03-03 23:14:58 +0000

    New fields to support exitgrp/waits() functionality.  Function prototypes for exitgrp handling.

--- vsta/src/include/sys/proc.h
+++ vsta/src/include/sys/proc.h
@@ -30,6 +30,29 @@ struct pgrp {
 #define PG_GROWTH (20)		/* # slots pg_members[] grows by */
 
 /*
+ * Status a child leaves behind on exit()
+ */
+struct exitst {
+	ulong e_pid;		/* PID of exit() */
+	int e_code;		/* Argument to exit() */
+	ulong e_usr, e_sys;	/* CPU time in user and sys */
+	struct exitst *e_next;	/* Next in list */
+};
+
+/*
+ * An exit group.  All children of the same parent belong to the
+ * same exit group.  When children exit, they will leave an exit
+ * status message linked here if the parent is still alive.
+ */
+struct exitgrp {
+	struct proc *e_parent;	/* Pointer to parent of group */
+	sema_t e_sema;		/* Sema bumped on each exit */
+	struct exitst *e_stat;	/* Status of each exit() */
+	ulong e_refs;		/* # references (parent + children) */
+	lock_t e_lock;		/* Mutex for fiddling */
+};
+
+/*
  * The actual per-process state
  */
 struct proc {
@@ -55,6 +78,9 @@ struct proc {
 	voidfun p_handler;	/* Handler for events */
 	struct pgrp		/* Process group for proc */
 		*p_pgrp;
+	struct exitgrp		/* Exit groups */
+		*p_children,	/*  ...the one our children use */
+		*p_parent;	/*  ...the one we're a child of */
 };
 
 #ifdef KERNEL
@@ -72,6 +98,15 @@ extern struct pgrp *alloc_pgrp(void);
 extern notify(ulong, ulong, char *, int);
 #define NOTIFY_PG ((ulong)-1)
 
-#endif
+/*
+ * Functions for fiddling exit groups
+ */
+extern struct exitgrp *alloc_exitgrp(struct proc *);
+extern void deref_exitgrp(struct exitgrp *);
+extern void noparent_exitgrp(struct exitgrp *);
+extern void post_exitgrp(struct exitgrp *, struct proc *, int);
+extern struct exitst *wait_exitgrp(struct exitgrp *);
+
+#endif /* KERNEL */
 
 #endif /* _PROC_H */
