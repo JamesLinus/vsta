f34f12d6b03ff227f3a1ddb00b0bc5998abcb2ce
vandys <vandys>
1993-03-05 23:31:49 +0000

    Add prototypes for CWD state save/restore

--- vsta/src/include/std.h
+++ vsta/src/include/std.h
@@ -19,5 +19,8 @@ extern char *strdup(char *), *strchr(char *, char), *strrchr(char *, char),
 extern int fork(void), tfork(__voidfun);
 extern int bcopy(void *, void *, unsigned int),
 	bcmp(void *, void *, unsigned int);
+extern long __cwd_size(void);
+extern void __cwd_save(char *);
+extern char *__cwd_restore(char *);
 
 #endif /* _STD_H */
