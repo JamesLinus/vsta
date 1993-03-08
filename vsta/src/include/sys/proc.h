ae20ef0986b66f5a00d5cdb5a99961c5fd379bae
vandys <vandys>
1993-03-08 23:02:51 +0000

    Move waits() stuff out to <sys/wait.h>

--- vsta/src/include/sys/proc.h
+++ vsta/src/include/sys/proc.h
@@ -8,6 +8,7 @@
 #include <sys/param.h>
 #include <lib/llist.h>
 #include <sys/mutex.h>
+#include <sys/wait.h>
 
 /*
  * Permission bits for current process
@@ -28,29 +29,6 @@ struct pgrp {
 	sema_t pg_sema;		/* Mutex for pgrp */
 };
 #define PG_GROWTH (20)		/* # slots pg_members[] grows by */
-
-/*
- * Status a child leaves behind on exit()
- */
-struct exitst {
-	ulong e_pid;		/* PID of exit() */
-	int e_code;		/* Argument to exit() */
-	ulong e_usr, e_sys;	/* CPU time in user and sys */
-	struct exitst *e_next;	/* Next in list */
-};
-
-/*
- * An exit group.  All children of the same parent belong to the
- * same exit group.  When children exit, they will leave an exit
- * status message linked here if the parent is still alive.
- */
-struct exitgrp {
-	struct proc *e_parent;	/* Pointer to parent of group */
-	sema_t e_sema;		/* Sema bumped on each exit */
-	struct exitst *e_stat;	/* Status of each exit() */
-	ulong e_refs;		/* # references (parent + children) */
-	lock_t e_lock;		/* Mutex for fiddling */
-};
 
 /*
  * The actual per-process state
@@ -97,15 +75,6 @@ extern struct pgrp *alloc_pgrp(void);
  */
 extern notify(ulong, ulong, char *, int);
 #define NOTIFY_PG ((ulong)-1)
-
-/*
- * Functions for fiddling exit groups
- */
-extern struct exitgrp *alloc_exitgrp(struct proc *);
-extern void deref_exitgrp(struct exitgrp *);
-extern void noparent_exitgrp(struct exitgrp *);
-extern void post_exitgrp(struct exitgrp *, struct proc *, int);
-extern struct exitst *wait_exitgrp(struct exitgrp *);
 
 #endif /* KERNEL */
 
