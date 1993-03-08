825bb148aad02eb40289ca1b9e95268089d591ae
vandys <vandys>
1993-03-08 23:03:20 +0000

    Initial revision

---
+++ vsta/src/lib/shell.c
@@ -0,0 +1,31 @@
+/*
+ * shell.c
+ *	Shell interface stuff
+ */
+#include <sys/wait.h>
+
+/*
+ * system()
+ *	Launch a shell, return status
+ */
+system(char *cmd)
+{
+	char *argv[4];
+	long pid;
+	struct exitst w;
+
+	argv[0] = "sh";
+	argv[1] = "-c";
+	argv[2] = cmd;
+	argv[3] = 0;
+	pid = fork();
+	if (pid < 0) {
+		return(-1);
+	}
+	if (pid == 0) {
+		execv("/bin/sh", argv);
+		_exit(-1);
+	}
+	waits(&w);
+	return(w.e_code);
+}
