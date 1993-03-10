76e881656db7f895b7489b8d8af6b4cf25837b56
vandys <vandys>
1993-03-10 18:44:12 +0000

    Add getcwd() prototype

--- vsta/src/include/std.h
+++ vsta/src/include/std.h
@@ -22,5 +22,6 @@ extern int bcopy(void *, void *, unsigned int),
 extern long __cwd_size(void);
 extern void __cwd_save(char *);
 extern char *__cwd_restore(char *);
+extern char *getcwd(char *, int);
 
 #endif /* _STD_H */
