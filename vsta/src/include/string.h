89358512e884227ee549b00efdfb5e5e607a7d50
vandys <vandys>
1993-03-13 01:40:18 +0000

    Initial revision

---
+++ vsta/src/include/string.h
@@ -0,0 +1,21 @@
+#ifndef _STRING_H
+#define _STRING_H
+/*
+ * string.h
+ *	Definition for string handling functions
+ */
+
+/*
+ * Prototypes
+ */
+extern char *strcpy(char *, char *), *strncpy(char *, char *, int);
+extern char *strcat(char *, char *), *strncat(char *, char *, int);
+extern int strlen(char *), strcmp(char *, char *),
+	strncmp(char *, char *, int);
+extern void *memcpy(void *, void *, unsigned int);
+extern char *strchr(char *, char), *strrchr(char *, char);
+extern char *index(char *, char), *rindex(char *, char);
+extern char *strdup(char *);
+extern int bcmp(void *, void *, unsigned int);
+
+#endif /* _STRING_H */
