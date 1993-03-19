ab9f9c810b962724a4a718721500e82b2a1a8cdd
vandys <vandys>
1993-03-19 00:56:20 +0000

    First pass at !ICANON handling; restructuring to accomodate cooked and raw; add tcget/setattr.

--- vsta/src/lib/tty.c
+++ vsta/src/lib/tty.c
@@ -108,18 +108,172 @@ init_port(struct port *port)
 	}
 
 /*
+ * canon()
+ *	Do input processing when canonical input is set
+ */
+static
+canon(struct termios *t, TTYBUF *fp, struct port *port)
+{
+	char c, c2;
+	char echobuf[2];
+	struct msg m;
+
+	/*
+	 * Get ready to read from scratch
+	 */
+	echobuf[1] = 0;
+	fp->f_cnt = 0;
+	fp->f_pos = fp->f_buf;
+
+	/*
+	 * Loop getting characters
+	 */
+	for (;;) {
+		/*
+		 * Build request
+		 */
+		m.m_op = FS_READ|M_READ;
+		m.m_buf = &c2;
+		m.m_arg = m.m_buflen = sizeof(c2);
+		m.m_nseg = 1;
+		m.m_arg1 = 0;
+
+		/*
+		 * Errors are handled below.  Otherwise we
+		 * move to a local variable so the optimizer
+		 * can leave it in a register if it likes.
+		 */
+		if (msg_send(port->p_port, &m) != sizeof(c2)) {
+			return(-1);
+		}
+		c = c2;
+
+		/*
+		 * Null char--always store.  Null in c_cc[] means
+		 * an * inactive entry, so null characters would
+		 * cause some confusion.
+		 */
+		if (!c) {
+			addc(fp, c);
+			continue;
+		}
+
+		/*
+		 * ICRNL--map CR to NL
+		 */
+		if ((c == '\r') && (t->c_iflag & ICRNL)) {
+			c = '\n';
+		}
+
+		/*
+		 * Erase?
+		 */
+		if (c == t->c_cc[VERASE]) {
+			if (fp->f_cnt < 1) {
+				continue;
+			}
+			delc(fp);
+			echo("\b \b");	/* Not right for tab */
+			continue;
+		}
+
+		/*
+		 * Kill?
+		 */
+		if (c == t->c_cc[VKILL]) {
+			echo("\\\r\n");	/* Should be optional */
+			fp->f_cnt = 0;
+			fp->f_pos = fp->f_buf;
+			continue;
+		}
+
+		/*
+		 * Add the character
+		 */
+		addc(fp, c);
+
+		/*
+		 * Echo?
+		 */
+		if (t->c_lflag & ECHO) {
+			echobuf[0] = c;
+			echo(echobuf);
+		}
+
+		/*
+		 * End of line?
+		 */
+		if (c == t->c_cc[VEOL]) {
+			break;
+		}
+	}
+	return(0);
+}
+
+/*
+ * non_canon()
+ *	Do input when ICANON turned off
+ *
+ * This mode is complex and wonderful.  The code here does its best
+ * for the common cases (especially VMIN==1, VTIME==0!) but does not
+ * pretend to handle all those timing-sensitive modes.
+ */
+static
+non_canon(struct termios *t, TTYBUF *fp, struct port *port)
+{
+	int x;
+	char echobuf[2];
+	struct msg m;
+
+	/*
+	 * Set up
+	 */
+	echobuf[1] = 0;
+	fp->f_cnt = 0;
+	fp->f_pos = fp->f_buf;
+
+	/*
+	 * Unlimited time, potentially limited data
+	 */
+	if (t->c_cc[VTIME] == 0) {
+		/*
+		 * Build request.  Read as much as we can get.
+		 */
+		m.m_op = FS_READ|M_READ;
+		m.m_buf = fp->f_buf;
+		m.m_arg = m.m_buflen = BUFSIZ;
+		m.m_nseg = 1;
+		m.m_arg1 = 0;
+		x = msg_send(port->p_port, &m);
+		if (x <= 0) {
+			return(-1);
+		}
+
+		/*
+		 * Flag data buffered.  If ECHO, echo it back now.
+		 */
+		fp->f_cnt += x;
+		if (t->c_lflag & ECHO) {
+			(void)write(1, fp->f_buf, x);
+		}
+		return(0);
+	}
+
+	/*
+	 * Others not supported (yet)
+	 */
+	return(-1);
+}
+
+/*
  * __tty_read()
  *	Fill buffer from TTY-type device
  */
 __tty_read(struct port *port, void *buf, uint nbyte)
 {
-	char c, c2;
 	struct termios *t = &tty_state;
-	char echobuf[2];
 	TTYBUF *fp;
-	uint cnt = 0;
-	struct msg m;
-	int error = 0;
+	int error, cnt;
 
 	/*
 	 * Do one-time setup if needed, get pointer to state info
@@ -132,94 +286,12 @@ __tty_read(struct port *port, void *buf, uint nbyte)
 	 */
 	if (fp->f_cnt == 0) {
 		/*
-		 * Get ready to read from scratch
+		 * Non-canonical processing get its own routine
 		 */
-		echobuf[1] = 0;
-		fp->f_cnt = 0;
-		fp->f_pos = fp->f_buf;
-
-		/*
-		 * Loop getting characters
-		 */
-		for (;;) {
-			/*
-			 * Build request
-			 */
-			m.m_op = FS_READ|M_READ;
-			m.m_buf = &c2;
-			m.m_arg = m.m_buflen = sizeof(c2);
-			m.m_nseg = 1;
-			m.m_arg1 = 0;
-
-			/*
-			 * Errors are handled below.  Otherwise we
-			 * move to a local variable so the optimizer
-			 * can leave it in a register if it likes.
-			 */
-			if (msg_send(port->p_port, &m) != sizeof(c2)) {
-				error = -1;
-				break;
-			}
-			c = c2;
-
-			/*
-			 * Null char--always store.  Null in c_cc[] means
-			 * an * inactive entry, so null characters would
-			 * cause some confusion.
-			 */
-			if (!c) {
-				addc(fp, c);
-				continue;
-			}
-
-			/*
-			 * ICRNL--map CR to NL
-			 */
-			if ((c == '\r') && (t->c_iflag & ICRNL)) {
-				c = '\n';
-			}
-
-			/*
-			 * Erase?
-			 */
-			if (c == t->c_cc[VERASE]) {
-				if (fp->f_cnt < 1) {
-					continue;
-				}
-				delc(fp);
-				echo("\b \b");	/* Not right for tab */
-				continue;
-			}
-
-			/*
-			 * Kill?
-			 */
-			if (c == t->c_cc[VKILL]) {
-				echo("\\\r\n");	/* Should be optional */
-				fp->f_cnt = 0;
-				fp->f_pos = fp->f_buf;
-				continue;
-			}
-
-			/*
-			 * Add the character
-			 */
-			addc(fp, c);
-
-			/*
-			 * Echo?
-			 */
-			if (t->c_lflag & ECHO) {
-				echobuf[0] = c;
-				echo(echobuf);
-			}
-
-			/*
-			 * End of line?
-			 */
-			if (c == t->c_cc[VEOL]) {
-				break;
-			}
+		if ((t->c_lflag & ICANON) == 0) {
+			error = non_canon(t, fp, port);
+		} else {
+			error = canon(t, fp, port);
 		}
 	}
 
@@ -267,5 +339,45 @@ __tty_write(struct port *port, void *buf, uint nbyte)
 __tty_close(struct port *port)
 {
 	free(port->p_data);
+	return(0);
+}
+
+/*
+ * tcsetattr()
+ *	Set TTY attributes
+ *
+ * This needs rethinking when I put a TTY monitor on top.  We still
+ * fiddle the state in the FDL, but we also have to pass some stuff
+ * up to our monitor.  For instance, the monitor needs to know what
+ * the interrupt characters are so he can spot them and send a signal
+ * to the appropriate process group.  wstat() will be the right way,
+ * but have to think what format the wstat() message would use, so leave
+ * until I'm ready to work on the monitor code.
+ */
+tcsetattr(int fd, int flag, struct termios *t)
+{
+	struct port *port;
+
+	port = __port(fd);
+	if (!port || (port->p_read != __tty_read)) {
+		return(-1);
+	}
+	tty_state = *t;
+	return(0);
+}
+
+/*
+ * tcgetattr()
+ *	Get current TTY attributes
+ */
+tcgetattr(int fd, struct termios *t)
+{
+	struct port *port;
+
+	port = __port(fd);
+	if (!port || (port->p_read != __tty_read)) {
+		return(-1);
+	}
+	*t = tty_state;
 	return(0);
 }
