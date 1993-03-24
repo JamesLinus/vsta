b3d0a4158f49e0adb144b71a0fe96ad067557b6a
vandys <vandys>
1993-03-24 00:38:51 +0000

    Initial revision

---
+++ vsta/src/bin/cmds/id.c
@@ -0,0 +1,65 @@
+/*
+ * id.c
+ *	Print out our current IDs
+ */
+#include <sys/param.h>
+#include <sys/perm.h>
+
+main()
+{
+	int x, y, disabled, printed = 0;
+	struct perm perm;
+
+	/*
+	 * Get ID and print
+	 */
+	for (x = 0; x < PROCPERMS; ++x) {
+		/*
+		 * Get next slot
+		 */
+		if (perm_ctl(x, (void *)0, &perm) < 0) {
+			continue;
+		}
+
+		/*
+		 * Clear disabled flag, then see if this slot
+		 * has anything worth printing.
+		 */
+		disabled = 0;
+		if (!PERM_ACTIVE(&perm)) {
+			if (PERM_DISABLED(&perm)) {
+				disabled = 1;
+				PERM_ENABLE(&perm);
+			} else {
+				continue;
+			}
+		}
+
+		/*
+		 * Comma separated
+		 */
+		if (printed > 0) {
+			printf(", ");
+		}
+		printed++;
+
+		/*
+		 * Print digits
+		 */
+		for (y = 0; y < PERM_LEN(&perm); ++y) {
+			if (y > 0) {
+				printf(".");
+			}
+			printf("%d", perm.perm_id[y]);
+		}
+
+		/*
+		 * Valid but currently disabled
+		 */
+		if (disabled) {
+			printf("(disabled)");
+		}
+	}
+	printf("\n");
+	return(0);
+}
