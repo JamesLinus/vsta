d3115542e2b8bc7979a39c42e93dc06dcfe622ec
vandys <vandys>
1993-04-08 17:48:54 +0000

    Add access()

--- vsta/src/lib/perm.c
+++ vsta/src/lib/perm.c
@@ -5,6 +5,8 @@
  * This is mostly a study in frustration; the POSIX style of interface
  * disables much of the expressive power of the VSTa protection system.
  */
+#include <unistd.h>
+#include <fcntl.h>
 
 /*
  * umask()
@@ -22,4 +24,27 @@ umask(int newmask)
 chmod(char *file, int mode)
 {
 	return(0);
+}
+
+/*
+ * access()
+ *	Tell if we can access a file
+ *
+ * Ignores the effective/real dichotomy, since we don't really
+ * have it as such.
+ */
+access(char *file, int mode)
+{
+	int fd;
+
+	if (mode & W_OK) {
+		fd = open(file, O_READ|O_WRITE);
+	} else {
+		fd = open(file, O_READ);
+	}
+	if (fd >= 0) {
+		close(fd);
+		return(0);
+	}
+	return(-1);
 }
