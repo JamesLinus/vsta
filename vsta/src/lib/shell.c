0750c084b77bf07a938e7bc74466ce26a57043d4
vandys <vandys>
1993-04-12 20:55:31 +0000

    Add path-oriented functions

--- vsta/src/lib/shell.c
+++ vsta/src/lib/shell.c
@@ -3,6 +3,7 @@
  *	Shell interface stuff
  */
 #include <sys/wait.h>
+#include <std.h>
 
 /*
  * system()
@@ -23,9 +24,85 @@ system(char *cmd)
 		return(-1);
 	}
 	if (pid == 0) {
-		execv("/bin/sh", argv);
+		execv("/vsta/bin/sh", argv);
 		_exit(-1);
 	}
 	waits(&w, 1);
 	return(w.e_code);
+}
+
+/*
+ * execvp()
+ *	Interface to execv() with path honored
+ */
+execvp(char *prog, char **argv)
+{
+	char *pathbuf, *path, *p, *buf;
+	int len;
+
+	/*
+	 * Absolute doesn't use path
+	 */
+	if ((prog[0] == '/') || !strncmp(prog, "./", 2) ||
+			!strncmp(prog, "../", 3)) {
+		return(execv(prog, argv));
+	}
+
+	/*
+	 * Try to find path from environment.  Just use program
+	 * name as-is if we don't have a path.
+	 */
+	pathbuf = path = getenv("PATH");
+	if (path == 0) {
+		return(execv(prog, argv));
+	}
+
+	/*
+	 * Try each element
+	 */
+	len = strlen(prog);
+	do {
+		/*
+		 * Find next path element
+		 */
+		p = strchr(path, ';');
+		if (p == 0) {
+			p = strchr(path, ':');
+		}
+		if (p) {
+			*p++ = '\0';
+		}
+
+		/*
+		 * Get temp buffer for full path
+		 */
+		buf = malloc(len+strlen(path)+1);
+		if (buf == 0) {
+			return(-1);
+		}
+		sprintf(buf, "%s/%s", path, prog);
+		(void)execv(buf, argv);
+
+		/*
+		 * Didn't fly; free buffer and iterate
+		 */
+		free(buf);
+		path = p;
+	} while (path);
+
+	/*
+	 * All failed.  Free malloc()'ed buffer of PATH, and return
+	 * failure.
+	 */
+	free(pathbuf);
+	return(-1);
+}
+
+/*
+ * execlp()
+ *	execl(), with path
+ */
+execlp(char *path, char *arg0, ...)
+{
+	return(execvp(path, &arg0));
 }
