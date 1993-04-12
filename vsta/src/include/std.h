a29f1ef8242777c688b1b874fb3cd33baa51aef9
vandys <vandys>
1993-04-12 20:56:54 +0000

    Add prototypes for more interfaces, fix voidfun dec'l

--- vsta/src/include/std.h
+++ vsta/src/include/std.h
@@ -6,8 +6,7 @@
  *
  * See comments for unistd.h
  */
-
-typedef void (*__voidfun)();
+#include <sys/types.h>
 
 /*
  * Routine templates
@@ -16,7 +15,7 @@ extern void *malloc(unsigned int), *realloc(void *, unsigned int);
 extern void free(void *);
 extern char *strdup(char *), *strchr(char *, char), *strrchr(char *, char),
 	*index(char *, char), *rindex(char *, char), *strerror(void);
-extern int fork(void), tfork(__voidfun);
+extern int fork(void), tfork(voidfun);
 extern int bcopy(void *, void *, unsigned int),
 	bcmp(void *, void *, unsigned int);
 extern long __cwd_size(void);
@@ -24,5 +23,10 @@ extern void __cwd_save(char *);
 extern char *__cwd_restore(char *);
 extern char *getcwd(char *, int);
 extern int dup(int), dup2(int, int);
+extern int execl(char *, char *, ...), execv(char *, char **),
+	execlp(char *, char *, ...), execvp(char *, char **);
+extern char *getenv(char *);
+extern int setenv(char *, char *);
+extern pid_t getpid(void), gettid(void), getppid(void);
 
 #endif /* _STD_H */
