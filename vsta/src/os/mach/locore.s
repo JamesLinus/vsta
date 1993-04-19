a217757e4857bfea82afa3e13a963d649eaa5824
vandys <vandys>
1993-04-19 21:42:20 +0000

    Use longword-moves when possible in bcopy/bzero

--- vsta/src/os/mach/locore.s
+++ vsta/src/os/mach/locore.s
@@ -73,9 +73,17 @@ _bzero:	pushl	%edi
 	movl	0xC(%esp),%edi
 	movl	0x10(%esp),%ecx
 	xorl	%eax,%eax
+	cmpl	$4,%ecx			/* Zero longwords */
+	jb	1f
+	shrl	$2,%ecx			/* Scale to longword count */
 	rep
+	stosl
+	movl	0x10(%esp),%ecx		/* Calculate byte resid */
+	andl	$3,%ecx
+	jz	2f
+1:	rep				/* Zero bytes */
 	stosb
-	popl	%ecx
+2:	popl	%ecx
 	popl	%edi
 	ret
 
@@ -90,19 +98,36 @@ _bcopy:	pushl	%esi
 	movl	0x10(%esp),%esi
 	movl	0x14(%esp),%edi
 	movl	0x18(%esp),%ecx
-	cmpl	%edi,%esi
+
+	cmpl	%edi,%esi	/* Potential ripple in copy? */
 	jb	2f
-	rep		/* No ripple in forward copy */
+
+1:	cmpl	$4,%ecx		/* Move longwords */
+	jb	5f
+	shrl	$2,%ecx		/* Scale units */
+	rep
+	movsl
+	movl	0x18(%esp),%ecx
+	andl	$3,%ecx
+	jz	3f
+
+5:	rep			/* Resid copy of bytes */
 	movsb
 
-3:	popl	%ecx	/* Restore registers and return */
+3:	popl	%ecx		/* Restore registers and return */
 	popl	%edi
 	popl	%esi
 	ret
 
-2:			/* Potential ripple in forward; copy backwards */
+2:	addl	%ecx,%esi	/* If no overlap, still copy forward */
+	cmpl	%edi,%esi
+	jae	4f
+	movl	0x10(%esp),%esi	/* Restore register */
+	jmp	1b		/* Forward copy */
+
+4:				/* Overlap; copy backwards */
 	std
-	addl	%ecx,%esi
+	/* addl	%ecx,%esi	Done in overlap check */
 	decl	%esi
 	addl	%ecx,%edi
 	decl	%edi
