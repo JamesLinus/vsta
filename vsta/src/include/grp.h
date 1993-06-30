1a7f84dc5e2da4264346d9282b45896fd8295044
vandys <vandys>
1993-06-30 21:25:47 +0000

    Initial revision

---
+++ vsta/src/include/grp.h
@@ -0,0 +1,32 @@
+#ifndef _GRP_H
+#define _GRP_H
+/*
+ * grp.h
+ *	"groups", ala VSTa
+ *
+ * Much of the POSIX group concept is missing; we emulate
+ * what we can.
+ */
+
+/*
+ * Numeric group ID
+ */
+typedef unsigned long gid_t;
+
+/*
+ * Group information
+ */
+struct group {
+	char *gr_name;		/* Name of group */
+	gid_t gr_gid;		/* GID */
+	char **gr_mem;		/* Members (not tabulated) */
+	char *gr_ids;		/* List of IDs granted this group */
+};
+
+/*
+ * Procedures
+ */
+extern struct group *getgrgid(gid_t),
+	*getgrnam(char *);
+
+#endif /* _GRP_H */
---
+++ vsta/src/include/pwd.h
@@ -0,0 +1,29 @@
+#ifndef _PWD_H
+#define _PWD_H
+/*
+ * pwd.h
+ *	Password file functions
+ *
+ * This represents a subset of what VSTa puts in the passwd file
+ */
+#include <grp.h>	/* for gid_t */
+
+/*
+ * Types
+ */
+typedef unsigned long uid_t;
+struct passwd {
+	char *pw_name;		/* Name of account */
+	uid_t pw_uid;		/* UID */
+	gid_t pw_gid;		/* GID */
+	char *pw_dir;		/* Home dir */
+	char *pw_shell;		/* Shell */
+};
+
+/*
+ * Prototypes
+ */
+extern struct passwd *getpwuid(uid_t),
+	*getpwnam(char *);
+
+#endif /* _PWD_H */
