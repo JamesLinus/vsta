40250da1e6470b659b89a09a8a10536aad8a5916
vandys <vandys>
1993-07-09 18:37:31 +0000

    Place arguments, if any, into boot server reserved area of the a.out.

--- vsta/src/boot.386/main.c
+++ vsta/src/boot.386/main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <alloc.h>
 #include <string.h>
+#include <memory.h>
 #include "boot.h"
 
 extern ushort move_jump_len;	/* Assembly support */
@@ -53,6 +54,88 @@ round_pbase(void)
 	 * Point to next chunk of memory, rounded up to a page
 	 */
 	pbase = (pbase + (NBPG-1)) & ~(NBPG-1);
+}
+
+/*
+ * try_setarg()
+ *	Try to insert the given argument into the area reserved for it
+ *
+ * This is a very basic argument line being passed; we don't honor
+ * quotes or any such nonsense.
+ *
+ * The layout of the memory area is:
+ *	32 bytes a.out header
+ *	8 bytes of a jump around the argument patch area to L1
+ *	argc
+ *	0xDEADBEEF (argv[0])
+ *	MAXARG << 16 | ARGSIZE (argv[1])
+ *	argv[2]
+ *	...
+ *	arg string area (ARGSIZE bytes)
+ * L1:	<rest of text segment>
+ */
+static void
+try_setarg(ulong base, char *p)
+{
+	char *buf = p;
+	int maxnarg, maxarg, argoff, len = strlen(p)+1;
+	ulong *lp;
+	const ulong headsz = 32L+8L;
+
+	/*
+	 * Skip a.out header and initial jmp instruction.  Keep a pointer
+	 * to the memory.
+	 */
+	base = base + headsz;
+	lp = lptr(base);
+
+	/*
+	 * Verify that the dummy area exists; otherwise we're trying
+	 * to pass boot arguments to a process not linked for it.
+	 */
+	if (lp[1] != 0xDEADBEEFL) {
+		printf("\nError: not linked for boot arguments.\n");
+		exit(1);
+	}
+
+	/*
+	 * Extract maxnarg and maxarg.  Calculate offset to base
+	 * of string area.
+	 */
+	maxnarg = lp[2] & 0xFFFF;
+	maxarg = (lp[2] >> 16) & 0xFFFF;
+	argoff = sizeof(long) + maxnarg*sizeof(long);
+
+	/*
+	 * Make sure it'll fit
+	 */
+	if (len > maxarg) {
+		printf("\nError: arguments too long.\n");
+		exit(1);
+	}
+
+	/*
+	 * Fill in argv while advancing argc.  In the process,
+	 * convert our argument strings to null-termination.
+	 */
+	while (p) {
+		if (lp[0] >= maxnarg) {
+			printf("\nError: too many arguments.\n");
+			exit(1);
+		}
+		lp[lp[0]+1] =		/* argv */
+			(p-buf)+argoff+NBPG+headsz;
+		p = strchr(p, ' ');
+		if (p) {
+			*p++ = '\0';
+		}
+		lp[0] += 1;		/* argc */
+	}
+
+	/*
+	 * Blast the buffer down into place, just beyond argc+argv
+	 */
+	memcpy(&lp[maxnarg+1], buf, len);
 }
 
 main(int argc, char **argv)
@@ -144,14 +227,72 @@ main(int argc, char **argv)
 	round_pbase();
 	bootbase = pbase;
 	while (fgets(buf, sizeof(buf)-1, bootf)) {
+		char *p;
+		ulong opbase;
+
+		/*
+		 * Convert to null-termination
+		 */
 		buf[strlen(buf)-1] = '\0';
+
+		/*
+		 * Skip blank lines and comments
+		 */
+		if (!buf[0] || (buf[0] == '#')) {
+			continue;
+		}
+
+		/*
+		 * Record arg string, if any
+		 */
+		p = strchr(buf, ' ');
+		if (p) {
+			*p = '\0';
+		}
+
+		/*
+		 * Load file
+		 */
 		if (freopen(buf, "rb", fp) == NULL) {
 			perror(buf);
 			exit(1);
 		}
 		printf(" %s", basename(buf)); fflush(stdout);
+
+		/*
+		 * Round to next page, load file
+		 */
 		round_pbase();
+		opbase = pbase;
 		load_image(fp);
+
+		/*
+		 * If there was an arg string, try to insert
+		 * it in the boot file
+		 */
+		if (p) {
+			char *q;
+
+			/*
+			 * Skip any leading path on the command name.
+			 * Saves precious bytes in the a.out argument
+			 * area.
+			 */
+			q = strrchr(buf, '/');
+			if (q) {
+				++q;
+			} else {
+				q = buf;
+			}
+
+			/*
+			 * Put back the space in the space-delinated
+			 * command line.  Put the arguments into place
+			 * in the memory image.
+			 */
+			*p = ' ';
+			try_setarg(opbase, q);
+		}
 	}
 	fclose(fp);
 	fclose(bootf);
