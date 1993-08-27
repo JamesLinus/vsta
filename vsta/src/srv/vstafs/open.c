1ab981b34b94e1462d526963cb97ba9fc490f492
vandys <vandys>
1993-08-27 16:46:57 +0000

    First pass, file creation

--- vsta/src/srv/vstafs/open.c
+++ vsta/src/srv/vstafs/open.c
@@ -3,6 +3,7 @@
  *	Routines for opening, closing, creating  and deleting files
  */
 #include <vstafs/vstafs.h>
+#include <vstafs/alloc.h>
 #include <std.h>
 
 /*
@@ -15,6 +16,20 @@ static struct openfile *
 findent(struct fs_dirent *d, uint nent, char *name)
 {
 	for ( ; nent > 0; ++d,--nent) {
+		/*
+		 * No more entries in file
+		 */
+		if (d->fs_clstart == 0) {
+			return(0);
+		}
+
+		/*
+		 * Deleted file
+		 */
+		if (d->fs_name[0] & 0x80) {
+			continue;
+		}
+
 		/*
 		 * Keep going while no match
 		 */
@@ -77,6 +92,15 @@ dir_lookup(struct buf *b, struct fs_file *fs, char *name)
 			d = index_buf(b2, 0, len);
 
 			/*
+			 * Special case for initial data in file
+			 */
+			if (x == 0) {
+				d = (struct fs_dirent *)
+					((char *)d + sizeof(struct fs_file));
+				len -= sizeof(struct fs_file);
+			}
+
+			/*
 			 * Look for our filename
 			 */
 			o = findent(d, len/sizeof(struct fs_dirent), name);
@@ -89,12 +113,169 @@ dir_lookup(struct buf *b, struct fs_file *fs, char *name)
 }
 
 /*
+ * create_file()
+ *	Create the initial "file" contents
+ *
+ *
+static struct openfile *
+create_file(uint type)
+{
+	daddr_t da;
+	struct buf *b;
+	struct fs_dirent *d;
+
+	/*
+	 * Get the block, map it
+	 */
+	da = alloc_block(1);
+	if (da == 0) {
+		return(0);
+	}
+	b = find_buf(da, 1);
+	if (b == 0) {
+		free_block(da, 1);
+		return(0);
+	}
+	d = index_buf(b, 0, 1);
+
+	/*
+	 * Fill in the fields
+	 */
+	d->fs_clstart = da;
+	d->fs_prev = 0;
+	d->fs_rev = 1;
+	d->fs_len = 0;
+	d->fs_type = type;
+	d->fs_nlink = 1;
+	d->fs_prot = XXX
+	d->fs_nblk = 1;
+	d->fs_blks[0].a_start = da;
+	d->fs_blks[0].a_len = 1;
+
+	/*
+	 * Allocate an openfile to it
+	 */
+	o = get_node(da);
+	if (o == 0) {
+		free_block(da, 1);
+	}
+
+	/*
+	 * Flush out info to disk, and return openfile
+	 */
+	dirty_buf(b);
+	sync_buf(b);
+	return(o);
+}
+
+/*
  * dir_newfile()
  *	Create a new entry in the current directory
+ *
+ * "b" is locked throughout.
  */
 static struct openfile *
 dir_newfile(struct buf *b, struct fs_file *fs, char *name, int type)
 {
+	ulong off;
+	struct buf *b2;
+	uint extent;
+	struct openfile *o;
+
+	/*
+	 * Get the openfile first
+	 */
+
+	o = create_file(type);
+
+	/*
+	 * Walk the directory entries one extent at a time
+	 */
+	for (extent = 0; extent < fs->fs_nblk; ++extent) {
+		uint x, len;
+		struct alloc *a = &fs->fs_blks[extent];
+
+		/*
+		 * Walk through an extent one buffer-full at a time
+		 */
+		for (x = 0; x < a->a_len; x += EXTSIZ) {
+			struct fs_dirent *d;
+
+			/*
+			 * Figure out size of next buffer-full
+			 */
+			len = a->a_len - x;
+			if (len > EXTSIZ) {
+				len = EXTSIZ;
+			}
+
+			/*
+			 * Map it in
+			 */
+			b2 = find_buf(a->a_start+x, len);
+			if (b2 == 0) {
+				return(0);
+			}
+			d = index_buf(b2, 0, len);
+
+			/*
+			 * Special case for initial data in file
+			 */
+			if (x == 0) {
+				d = (struct fs_dirent *)
+					((char *)d + sizeof(struct fs_file));
+				len -= sizeof(struct fs_file);
+			}
+
+			/*
+			 * Look for our filename
+			 */
+			d = findfree(d, len/sizeof(struct fs_dirent));
+			if (d) {
+				/*
+				 * Found a slot.  Put our data
+				 * into place, make sure it's registered
+				 * on-disk, and return an openfile.
+				 */
+				strcpy(d->d_name, name);
+				d->d_clstart = o->o_file;
+				dirty_buf(b2);
+				sync_buf(b2)
+				return(o);
+			}
+
+		}
+	}
+
+	/*
+	 * No luck with existing blocks.  Use bmap() to map in some
+	 * more storage.
+	 */
+	{
+		struct fs_dirent *dp;
+		uint size;
+
+		b2 = bmap(fs, fs->fs_len, sizeof(struct fs_dirent),
+			&dp, &size);
+		if (b2 == 0) {
+			return(0);
+		}
+		ASSERT_DEBUG(size >= sizeof(struct fs_dirent),
+			"dir_newfile: bad growth");
+
+		/*
+		 * Now we have a slot, so fill it in & return success
+		 */
+		strcpy(dp->d_name, name);
+		dp->d_clstart = o->o_file;
+		dirty_buf(b2);
+		sync_buf(b2)
+		return(o);
+	}
+
+	/*
+	 * We have failed.
+	return(0);
 }
 
 /*
@@ -130,7 +311,7 @@ vfs_open(struct msg *m, struct file *f)
 	 * Look up name, make sure "fs" stays valid
 	 */
 	lock_buf(b);
-	o = dir_lookup(b, fs, m->m_buf);
+	o = dir_lookup(b, fs, m->m_buf, findent);
 	unlock_buf(b);
 
 	/*
