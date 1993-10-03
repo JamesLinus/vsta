71d9b742f17011ca0233c2f79edfac8c681d2efa
vandys <vandys>
1993-10-03 20:37:54 +0000

    Always fill device using sector I/O

--- vsta/src/srv/vstafs/cmd/mkfs.c
+++ vsta/src/srv/vstafs/cmd/mkfs.c
@@ -5,8 +5,10 @@
 #include <sys/types.h>
 #include "../vstafs.h"
 #include <stdio.h>
+#include <std.h>
 
 static FILE *fp;	/* Device we're filling in */
+void *secbuf;		/* A utility sector buffer */
 
 /*
  * write_header()
@@ -15,14 +17,14 @@ static FILE *fp;	/* Device we're filling in */
 static void
 write_header(ulong nsec)
 {
-	struct fs f;
+	struct fs *f = secbuf;
 
 	fseek(fp, (off_t)BASE_SEC*SECSZ, 0);
-	f.fs_magic = FS_MAGIC;
-	f.fs_size = nsec;
-	f.fs_extsize = EXTSIZ;
-	f.fs_free = FREE_SEC;
-	fwrite(&f, sizeof(struct fs), 1, fp);
+	f->fs_magic = FS_MAGIC;
+	f->fs_size = nsec;
+	f->fs_extsize = EXTSIZ;
+	f->fs_free = FREE_SEC;
+	fwrite(f, SECSZ, 1, fp);
 }
 
 /*
@@ -32,25 +34,25 @@ write_header(ulong nsec)
 static void
 write_root(void)
 {
-	struct fs_file fs;
+	struct fs_file *fs = secbuf;
 
 	fseek(fp, (off_t)ROOT_SEC*SECSZ, 0);
-	fs.fs_prev = 0;
-	fs.fs_rev = 1;
-	fs.fs_len = sizeof(struct fs_file);
-	fs.fs_type = FT_DIR;
-	fs.fs_nlink = 1;
-	fs.fs_prot.prot_len = 2;
-	fs.fs_prot.prot_default = ACC_READ;
-	fs.fs_prot.prot_bits[0] = 0;
-	fs.fs_prot.prot_id[0] = 1;
-	fs.fs_prot.prot_bits[1] = ACC_WRITE|ACC_CHMOD;
-	fs.fs_prot.prot_id[1] = 1;
-	fs.fs_owner = 0;
-	fs.fs_nblk = 1;
-	fs.fs_blks[0].a_start = ROOT_SEC;
-	fs.fs_blks[0].a_len = 1;
-	fwrite(&fs, sizeof(struct fs_file), 1, fp);
+	fs->fs_prev = 0;
+	fs->fs_rev = 1;
+	fs->fs_len = sizeof(struct fs_file);
+	fs->fs_type = FT_DIR;
+	fs->fs_nlink = 1;
+	fs->fs_prot.prot_len = 2;
+	fs->fs_prot.prot_default = ACC_READ;
+	fs->fs_prot.prot_bits[0] = 0;
+	fs->fs_prot.prot_id[0] = 1;
+	fs->fs_prot.prot_bits[1] = ACC_WRITE|ACC_CHMOD;
+	fs->fs_prot.prot_id[1] = 1;
+	fs->fs_owner = 0;
+	fs->fs_nblk = 1;
+	fs->fs_blks[0].a_start = ROOT_SEC;
+	fs->fs_blks[0].a_len = 1;
+	fwrite(fs, SECSZ, 1, fp);
 }
 
 /*
@@ -60,15 +62,15 @@ write_root(void)
 static void
 write_freelist(ulong nsec)
 {
-	struct free f;
+	struct free *f = secbuf;
 
 	fseek(fp, (off_t)FREE_SEC*SECSZ, 0);
-	bzero(&f, sizeof(f));
-	f.f_next = 0;
-	f.f_nfree = 1;
-	f.f_free[0].a_start = FREE_SEC+1;
-	f.f_free[0].a_len = nsec-(FREE_SEC+1);
-	fwrite(&f, sizeof(struct free), 1, fp);
+	bzero(f, SECSZ);
+	f->f_next = 0;
+	f->f_nfree = 1;
+	f->f_free[0].a_start = FREE_SEC+1;
+	f->f_free[0].a_len = nsec-(FREE_SEC+1);
+	fwrite(f, SECSZ, 1, fp);
 }
 
 /*
@@ -86,7 +88,6 @@ int
 main(int argc, char **argv)
 {
 	ulong x, nsec;
-	char sec[SECSZ];
 	int prealloc = 0;
 
 	/*
@@ -114,11 +115,12 @@ main(int argc, char **argv)
 	/*
 	 * Preallocate sectors
 	 */
+	secbuf = malloc(SECSZ);
 	if (prealloc) {
-		bzero(sec, SECSZ);
+		bzero(secbuf, SECSZ);
 		printf("Pre-allocate: "); fflush(stdout);
 		for (x = 0; x < nsec; ++x) {
-			fwrite(sec, SECSZ, 1, fp);
+			fwrite(secbuf, SECSZ, 1, fp);
 			/* Put a marker each 8K */
 			if ((x % ((8*1024)/SECSZ)) == 0) {
 				static int col = 0;
