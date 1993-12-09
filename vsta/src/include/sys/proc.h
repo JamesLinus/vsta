30218bdec8b50f976a662cf37f8d7b788b13f6f4
vandys <vandys>
1993-12-09 06:19:23 +0000

    Add ptrace support

--- vsta/src/include/sys/proc.h
+++ vsta/src/include/sys/proc.h
@@ -4,11 +4,14 @@
  * proc.h
  *	Per-process data
  */
+#define PROC_DEBUG /* Define for process debugging support */
+
 #include <sys/perm.h>
 #include <sys/param.h>
 #include <llist.h>
 #include <sys/mutex.h>
 #include <sys/wait.h>
+#include <mach/machreg.h>
 
 /*
  * Permission bits for current process
@@ -29,6 +32,55 @@ struct pgrp {
 	sema_t pg_sema;		/* Mutex for pgrp */
 };
 #define PG_GROWTH (20)		/* # slots pg_members[] grows by */
+
+#ifdef PROC_DEBUG
+/*
+ * Communication area between debugger and slave
+ */
+struct pdbg {
+	port_t pd_port;		/* Debugger serves this port */
+	port_name pd_name;	/*  ...portname for port */
+	uint pd_flags;		/* See below */
+};
+
+/* Bits in pd_flags */
+#define PD_ALWAYS 1		/* Stop at next chance */
+#define PD_SYSCALL 2		/*  ...before & after syscalls */
+#define PD_EVENT 4		/*  ...when receiving an event */
+#define PD_EXIT 8		/*  ...when exiting */
+#define PD_BPOINT 16		/*  ...breakpoint reached */
+#define PD_CONNECTING 0x8000	/* Slave has connect in progress */
+
+/*
+ * Values for m_op of a debug message.  The sense of the operation
+ * is further defined by m_arg and m_arg1.
+ */
+#define PD_SLAVE 300		/* Slave ready for commands */
+#define PD_RUN 301		/* Run */
+#define PD_STEP 302		/* Run one instruction, then break */
+#define PD_BREAK 303		/* Set/clear breakpoint */
+#define PD_REGS 304		/* Get/put registers */
+#define PD_MASK 305		/* Set mask */
+#define PD_RDMEM 306		/* Read memory */
+#define PD_WRMEM 307		/*  ...write */
+
+/*
+ * Handy macro for checking if we should drop into debug slave
+ * mode.
+ */
+#define PTRACE_PENDING(p, fl, ev) \
+	if ((p)->p_dbg.pd_flags & ((fl) | PD_ALWAYS)) { \
+		ptrace_slave(ev); \
+	}
+
+#else
+
+/*
+ * Just stub this for non-debugging kernel
+ */
+#define PTRACE_PENDING(p, fl, ev)
+
+#endif /* PROC_DEBUG */
 
 /*
  * The actual per-process state
@@ -61,6 +113,11 @@ struct proc {
 		*p_parent;	/*  ...the one we're a child of */
 	char p_cmd[8];		/* Command name (untrusted) */
 	char p_event[ERRLEN];	/* Event which killed us */
+#ifdef PROC_DEBUG
+	struct pdbg p_dbg;	/* Who's debugging us (if anybody) */
+	struct dbg_regs		/* Debug register state */
+		p_dbgr;		/*  valid if T_DEBUG active */
+#endif
 };
 
 #ifdef KERNEL
