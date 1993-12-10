cc42f17167730977ffb49a1411709d06063e016b
vandys <vandys>
1993-12-10 19:13:06 +0000

    Initial revision

---
+++ vsta/src/bin/adb/i386.c
@@ -0,0 +1,47 @@
+/*
+ * i386.c
+ *	Routines which know all the gore of the i386
+ */
+#include <sys/types.h>
+#include <mach/machreg.h>
+
+extern char *nameval(ulong);
+extern void getregs(struct trapframe *);
+
+/*
+ * flagnames()
+ *	Return string describing flags
+ */
+static char *
+flagnames(ulong eflags)
+{
+	static char buf[60];
+
+	buf[0] = '\0';
+	if (eflags & F_CF) strcat(buf, " carry");
+	if (eflags & F_PF) strcat(buf, " parity");
+	if (eflags & F_AF) strcat(buf, " bcd");
+	if (eflags & F_ZF) strcat(buf, " zero");
+	if (eflags & F_SF) strcat(buf, " sign");
+	if (eflags & F_DF) strcat(buf, " dir");
+	if (eflags & F_OF) strcat(buf, " overflow");
+	return(buf);
+}
+
+/*
+ * dump_regs()
+ *	Print out registers
+ */
+void
+dump_regs(void)
+{
+	struct trapframe t;
+
+	getregs(&t);
+	printf("eip 0x%x (%s) eflags %s\n",
+		t.eip, nameval(t.eip), flagnames(t.eflags));
+	printf(" eax 0x%x ebx 0x%x ecx 0x%x edx 0x%x esi 0x%x edi 0x%x\n",
+		t.eax, t.ebx, t.ecx, t.edx, t.esi, t.edi);
+	printf(" esp 0x%x ebp 0x%x\n",
+		t.esp, t.ebp);
+}
