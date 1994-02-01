c186ecc785cdf6076048a1756f3f288fb881f36a
vandys <vandys>
1994-02-01 23:14:51 +0000

    Fix commenting

--- vsta/src/include/mach/pit.h
+++ vsta/src/include/mach/pit.h
@@ -5,16 +5,22 @@
  *	Constants relating to the 8254 (or 8253) programmable interval timers
  */
  
-/* Port address of the control port and timer channels */
+/*
+ * Port address of the control port and timer channels
+ */
 #define PIT_CTRL 0x43
 #define PIT_CH0 0x40
 #define PIT_CH1 0x41
 #define PIT_CH2 0x42
 
-/* Command to set square wave clock mode */
+/*
+ * Command to set square wave clock mode
+ */
 #define CMD_SQR_WAVE 0x36
 
-/* The internal tick rate in ticks per second */
+/*
+ *The internal tick rate in ticks per second
+ */
 #define PIT_TICK 1193180
 
 #endif /* _MACHPIT_H */
