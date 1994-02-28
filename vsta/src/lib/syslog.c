39bcec3599441fc6bb367c2757d56644d30194d6
vandys <vandys>
1994-02-28 20:02:13 +0000

    Initial revision

---
+++ vsta/src/lib/syslog.c
@@ -0,0 +1,55 @@
+/*
+ * syslog.c
+ *	A (very) poor man's syslog handler
+ */
+#include <sys/fs.h>
+#include <syslog.h>
+
+/*
+ * Our bane of existence, varargs, and our way to avoid it
+ */
+#define ARG(start, idx) (*((void **)&(start) + (idx)))
+
+/*
+ * levelmsg()
+ *	Convert numeric level to string
+ */
+static char *
+levelmsg(int level)
+{
+	switch (level) {
+	case LOG_EMERG: return("emergency");
+	case LOG_ALERT: return("alert");
+	case LOG_CRIT: return("critical");
+	case LOG_ERR: return("error");
+	case LOG_WARNING: return("warnings");
+	case LOG_NOTICE: return("notice");
+	case LOG_INFO: return("info");
+	case LOG_DEBUG: return("debug");
+	default: return("unknown");
+	}
+}
+
+/*
+ * syslog()
+ *	Report error conditions
+ *
+ * We just dump them to the console
+ */
+void
+syslog(int level, char *msg, ...)
+{
+	port_t p;
+	int fd;
+	char buf[256];
+
+	p = path_open("CONS:0", ACC_WRITE);
+	if (p < 0) {
+		return;
+	}
+	fd = __fd_alloc(p);
+	sprintf(buf, "syslog: %s: ", levelmsg(level));
+	sprintf(buf, msg, ARG(msg, 1), ARG(msg, 2), ARG(msg, 3));
+	write(fd, buf, strlen(buf));
+	close(fd);
+}
