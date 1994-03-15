d031af9822342505a93224d404cc5987b21896ca
vandys <vandys>
1994-03-15 21:58:37 +0000

    Provide appropriate treatment for cheated and full dispatch

--- vsta/src/os/kern/sched.c
+++ vsta/src/os/kern/sched.c
@@ -303,11 +303,12 @@ swtch(void)
 
 	/*
 	 * If we've picked the same guy, don't need to do anything fancy.
-	 * Since he paid full dues to be here, give him another CPU
-	 * quanta.
 	 */
 	if (s->s_thread == curthread) {
-		curthread->t_runticks = RUN_TICKS;
+		if (pri != PRI_CHEATED) {
+			curthread->t_runticks = RUN_TICKS;
+		}
+		cpu.pc_pri = pri;
 		v_lock(&runq_lock, SPL0);
 		return;
 	}
