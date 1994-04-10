9cbb911fe65d6fc2bfa9ec9a8bc5d1ad9530528c
vandys <vandys>
1994-04-10 19:54:12 +0000

    Support "fast" mkfs'ing, not writing all blocks

--- vsta/src/srv/bfs/cmd/mkbfs.c
+++ vsta/src/srv/bfs/cmd/mkbfs.c
@@ -2,19 +2,23 @@
  * Filename:	mkbfs.c
  * Originated:	Andy Valencia
  * Updated By:	Dave Hudson <dave@humbug.demon.co.uk>
- * Last Update: 11th February 1994
+ * Last Update: 8th April 1994
  * Implemented:	GNU GCC version 2.5.7
  *
- * Description: Create a BFS filesystem
+ * Description: Create a bfs filesystem
  *
- * This guy assumes he can just fopen(..., "w") the named file, and make
- * himself a filesystem.  He writes all the blocks, which is probably not
- * desirable for a native mkfs.  This does however allow a bfs to be built
- * on top of an existing fs - very useful for avoiding repartitioning, or
- * for running bfs where no fs partitioning mechanism exists (PC FDDs).
+ * We assume we can just fopen(..., "w") the named file, and make ourselves
+ * a filesystem.  We writes all of the blocks if a fs size is specified on the
+ * command line, which is probably not desirable for a "mkfs" util.  This does
+ * however allow a bfs to be built on top of an existing fs - very useful for
+ * avoiding repartitioning, or for running bfs where no fs partitioning
+ * mechanism exists (PC FDDs).  If no blocks parameter is spec'd we try and
+ * determine the fs size automagically!
  */
 
 
+#include <fdl.h>
+#include <stat.h>
 #include <std.h>
 #include <stdio.h>
 #include <string.h>
@@ -26,39 +30,58 @@ void
 main(int argc, char **argv)
 {
 	FILE *fp;
+	int fd;
+	int write_all_blocks = 0;
 	struct super sb;
 	struct dirent d;
 	uchar block[BLOCKSIZE];
-	int x, nblocks, dblocks = 4;
+	int x, nblocks, zblocks, dblocks = 8;
+	char *statstr;
 
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s <device> <blocks>\n", argv[0]);
+	if (argc != 3 && argc != 2) {
+		fprintf(stderr,
+			"Usage: %s <device_or_fs_file> [number_of_blocks]\n",
+			argv[0]);
 		exit(1);
 	}
-	if (sscanf(argv[2], "%d", &nblocks) != 1) {
-		fprintf(stderr, "Illegal number: %s\n", argv[2]);
-		exit(1);
-	}
+
 	if ((fp = fopen(argv[1], "wb")) == NULL) {
 		perror(argv[1]);
 		exit(1);
 	}
 
 	/*
-	 * Zero out the whole thing first.
+	 * How many fs blocks are we going for?
 	 */
-	memset(block, '\0', BLOCKSIZE);
-	for (x = 0; x < nblocks; ++x) {
-		if (fwrite(block, sizeof(char), BLOCKSIZE, fp)
-		    		!= BLOCKSIZE) {
-			perror(argv[1]);
+	if (argc == 2) {
+		/*
+		 * Try to work out the number of fs blocks via a stat call
+		 */
+		fd = fileno(fp);
+		statstr = rstat(__fd_port(fd), "size");
+		if (statstr == NULL ) {
+			fprintf(stderr,
+				"Unable to stat size of: %s\n", argv[1]);
 			exit(1);
 		}
+		sscanf(statstr, "%d", &nblocks);
+		nblocks /= BLOCKSIZE;
+	} else {
+		/*
+		 * Get the number of fs blocks off the command line
+		 */
+		if (sscanf(argv[2], "%d", &nblocks) != 1) {
+			fprintf(stderr,
+				"Illegal number of blocks: %s\n", argv[2]);
+			exit(1);
+		}
+		write_all_blocks = 1;
 	}
-	fseek(fp, 0, SEEK_SET);
+
+	printf("Creating a 'bfs' fs on %s of %d blocks\n", argv[1], nblocks);
 
 	/*
-	 * Create a superblock and write it out
+	 * Create superblock details and run sanity checks
 	 */
 	sb.s_magic = SMAGIC;
 	sb.s_blocks = nblocks;
@@ -71,10 +94,38 @@ main(int argc, char **argv)
 	sb.s_nextfree = dblocks + sb.s_dirstart;
 	sb.s_ndirents = dblocks * (BLOCKSIZE / sizeof(struct dirent));
 	sb.s_direntsize = sizeof(struct dirent);
-	if (fwrite(&sb, sizeof(sb), 1, fp) != 1) {
+
+	if (sb.s_datablocks < MINDATABLOCKS) {
+		fprintf(stderr,
+			"Too few data blocks (%d) with %d fs blocks\n",
+			sb.s_datablocks, nblocks);
+		exit(1);
+	}
+
+	/*
+	 * Zero out the superblock, dir space and possibly all of the fs space
+	 */
+	memset(block, '\0', BLOCKSIZE);
+	zblocks = write_all_blocks ? nblocks : sb.s_supblocks + dblocks;
+	for (x = 0; x < zblocks; ++x) {
+		fseek(fp, x * BLOCKSIZE, SEEK_SET);
+		if (fwrite(block, sizeof(char), BLOCKSIZE, fp)
+		    		!= BLOCKSIZE) {
+			perror(argv[1]);
+			exit(1);
+		}
+	}
+	fseek(fp, 0, SEEK_SET);
+
+	/*
+	 * Write the superblock
+	 */
+	memcpy(block, &sb, sizeof(struct super));
+	if (fwrite(block, BLOCKSIZE, 1, fp) != 1) {
 		perror(argv[1]);
 		exit(1);
 	}
+	fflush(fp);
 
 	/*
 	 * Write directory entries until we can't fit any more in
@@ -95,10 +146,11 @@ main(int argc, char **argv)
 			fseek(fp, endblk * BLOCKSIZE, SEEK_SET);
 		}
 		d.d_inum = x++;
-printf("%d %d!  ", d.d_inum, ftell(fp));
 		if (fwrite(&d, sizeof(d), 1, fp) != 1) {
 			perror(argv[1]);
 			exit(1);
 		}
 	}
+
+	fclose(fp);
 }
