85abc101febbe47cb8389a295ca61e3e1bdaded1
vandys <vandys>
1994-06-21 20:55:53 +0000

    Add openlog() and support for logging options

--- vsta/src/lib/syslog.c
+++ vsta/src/lib/syslog.c
@@ -5,6 +5,10 @@
 #include <sys/fs.h>
 #include <syslog.h>
 #include <stdio.h>
+#include <std.h>
+
+static char *id;		/* Name os process posting message */
+static int logopt, logfacil;	/* Logging options & facility */
 
 /*
  * levelmsg()
@@ -27,6 +31,40 @@ levelmsg(int level)
 }
 
 /*
+ * openlog()
+ *	Initialize for syslog(), record identity
+ */
+void
+openlog(char *ident, int opt, int facil)
+{
+	id = strdup(ident);
+	logopt = opt;
+	logfacil = facil;
+}
+
+/*
+ * pidstr()
+ *	Return string representation of PID
+ *
+ * Returns empty string of LOG_PID not specified
+ */
+static char *
+pidstr(void)
+{
+	static ulong pid;
+	static char str[16];
+
+	if ((logopt & LOG_PID) == 0) {
+		return("");
+	}
+	if (pid == 0) {
+		pid = getpid();
+		sprintf(str, "(pid %ld) ", pid);
+	}
+	return(str);
+}
+
+/*
  * syslog()
  *	Report error conditions
  *
@@ -46,7 +84,8 @@ syslog(int level, const char *msg, ...)
 	}
 	fd = __fd_alloc(p);
 	va_start(ap, msg);
-	sprintf(buf, "syslog: %s: ", levelmsg(level));
+	sprintf(buf, "syslog: %s %s%s: ",
+		id ? id : "", pidstr(), levelmsg(level));
 	vsprintf(buf + strlen(buf), msg, ap);
 	if (buf[strlen(buf)-1] != '\n') {
 		strcat(buf, "\n");
