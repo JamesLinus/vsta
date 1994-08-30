b9eeda0578624264b5813bdb61423039b16452fb
vandys <vandys>
1994-08-30 17:20:04 +0000

    Initial revision

---
+++ vsta/src/srv/ka9q/vsta.h
@@ -0,0 +1,27 @@
+#ifndef VSTA_H
+#define VSTA_H
+/*
+ * vsta.h
+ *	VSTa-specific definitions
+ */
+#include <sys/types.h>
+
+/*
+ * Our private hack mutex package
+ */
+typedef unsigned char lock_t;
+
+extern void init_lock(lock_t *), p_lock(volatile lock_t *),
+	v_lock(lock_t *);
+
+/*
+ * Shared data
+ */
+extern lock_t ka9q_lock;
+
+/*
+ * Functions
+ */
+extern void vsta_daemon(voidfun), do_mainloop(void);
+
+#endif /* VSTA_H */
