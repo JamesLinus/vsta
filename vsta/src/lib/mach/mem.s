2e068bab864b86d1b1c12e41eb774b61dd644cee
vandys <vandys>
1994-09-07 19:17:54 +0000

    Update bzero()/bcopy() to faster kernel versions, set up so same routines can be shared by kernel and libc.

--- vsta/src/lib/mach/mem.s
+++ vsta/src/lib/mach/mem.s
@@ -13,9 +13,19 @@ _bzero:	pushl	%edi
 	movl	0xC(%esp),%edi
 	movl	0x10(%esp),%ecx
 	xorl	%eax,%eax
+	cmpl	$4,%ecx			/* Zero longwords */
+	jb	1f
+	shrl	$2,%ecx			/* Scale to longword count */
+	cld
 	rep
+	stosl
+	movl	0x10(%esp),%ecx		/* Calculate byte resid */
+	andl	$3,%ecx
+	jz	2f
+1:	cld
+	rep				/* Zero bytes */
 	stosb
-	popl	%ecx
+2:	popl	%ecx
 	popl	%edi
 	ret
 
@@ -30,19 +40,38 @@ _bcopy:	pushl	%esi
 	movl	0x10(%esp),%esi
 	movl	0x14(%esp),%edi
 	movl	0x18(%esp),%ecx
-	cmpl	%edi,%esi
+
+	cmpl	%edi,%esi	/* Potential ripple in copy? */
 	jb	2f
-	rep		/* No ripple in forward copy */
+
+1:	cmpl	$4,%ecx		/* Move longwords */
+	jb	5f
+	shrl	$2,%ecx		/* Scale units */
+	cld
+	rep
+	movsl
+	movl	0x18(%esp),%ecx
+	andl	$3,%ecx
+	jz	3f
+
+5:	cld
+	rep			/* Resid copy of bytes */
 	movsb
 
-3:	popl	%ecx	/* Restore registers and return */
+3:	popl	%ecx		/* Restore registers and return */
 	popl	%edi
 	popl	%esi
 	ret
 
-2:			/* Potential ripple in forward; copy backwards */
+2:	addl	%ecx,%esi	/* If no overlap, still copy forward */
+	cmpl	%edi,%esi
+	jae	4f
+	movl	0x10(%esp),%esi	/* Restore register */
+	jmp	1b		/* Forward copy */
+
+4:				/* Overlap; copy backwards */
 	std
-	addl	%ecx,%esi
+	/* addl	%ecx,%esi	Done in overlap check */
 	decl	%esi
 	addl	%ecx,%edi
 	decl	%edi
@@ -50,13 +79,3 @@ _bcopy:	pushl	%esi
 	movsb
 	cld
 	jmp	3b
-
-/*
- * getpagesize()
- *	Easy to answer here, eh?
- */
-	.text
-	.globl	_getpagesize
-_getpagesize:
-	movl	$0x1000,%eax
-	ret
