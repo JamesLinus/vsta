c4941f71180797625f2a30520915f7f0c95c6d27
vandys <vandys>
1994-09-23 20:39:37 +0000

    Clean up mmap() support

--- vsta/src/include/sys/port.h
+++ vsta/src/include/sys/port.h
@@ -85,6 +85,7 @@ extern struct portref *find_portref(struct proc *, port_t),
 	*delete_portref(struct proc *, port_t);
 extern struct port *find_port(struct proc *, port_t),
 	*delete_port(struct proc *, port_t);
+extern void mmap_cleanup(struct port *);
 #endif
 
 #endif /* _PORT_H */
