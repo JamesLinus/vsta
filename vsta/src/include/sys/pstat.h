04e35993eafb73eacdd794a72703a5870c44b451
vandys <vandys>
1994-12-21 05:29:59 +0000

    pstat() enhancements

--- vsta/src/include/sys/pstat.h
+++ vsta/src/include/sys/pstat.h
@@ -5,29 +5,59 @@
  *	Definitions for pstat() process status query syscall
  */
 #include <sys/types.h>
+#include <sys/sched.h>
+#include <sys/perm.h>
+#include <sys/syscall.h>
+#include <mach/trap.h>
+#include <mach/isr.h>
 
 /*
- * pstat struct
+ * process status struct
  */
-struct pstat {
-	ulong ps_pid;		/* PID of process */
-	char ps_cmd[8];		/* Command name */
-	uint ps_nthread;	/* # threads under process */
-	uint ps_nsleep,		/* # threads sleeping */
-		ps_nrun,	/*  ...runnable */
-		ps_nonproc;	/*  ...on a CPU */
-	ulong ps_usrcpu,	/* Total CPU time used in user mode */
-		ps_syscpu;	/*  ...in kernel */
+struct pstat_proc {
+	ulong psp_pid;		/* PID of process */
+	char psp_cmd[8];	/* Command name */
+	uint psp_nthread;	/* Number of threads under process */
+	uint psp_nsleep;	/* Number of threads sleeping */
+	uint psp_nrun;		/*  ...runnable */
+	uint psp_nonproc;	/*  ...on a CPU */
+	ulong psp_usrcpu;	/* Total CPU time used in user mode */
+	ulong psp_syscpu;	/*  ...in kernel */
+	struct prot psp_prot;	/* Process protection info */
+	uchar psp_moves;	/* Does this process move in the list? */
 };
 
 /*
- * pstat()
- *	Get process status
- *
- * You pass it a pstat struct, a count of such structs, and the sizeof()
- * the struct.  This last allows the struct to grow but still be binary
- * compatible with old executables.
+ * System type identification string
  */
-extern int pstat(struct pstat *, uint, uint);
+#define PS_SYSID 16
+
+/*
+ * configuration/kernel status struct
+ */
+struct pstat_kernel {
+	ulong psk_memory;	/* Size of system RAM in bytes */
+	uint psk_ncpu;		/* Number of CPUs */
+	ulong psk_freemem;	/* Bytes of free memory */
+	struct time psk_uptime;	/* How long has the system been up? */
+	uint psk_runnable;	/* Number of runnable threads */
+};
+
+/*
+ * pstat status request types
+ */
+#define PSTAT_PROC 0
+#define PSTAT_PROCLIST 1
+#define PSTAT_KERNEL 2
+
+/*
+ * pstat()
+ *	Get status information out of the kernel
+ *
+ * You pass it a status type, two arguments, a pointer to a status
+ * structure and the sizeof() the struct.  This last allows the struct
+ * to grow but still be binary compatible with old executables.
+ */
+extern int pstat(uint, uint, void *, uint);
 
 #endif /* _PSTAT_H */
