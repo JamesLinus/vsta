3d0d337e54c92156494abc7f5e93307cafae8325
vandys <vandys>
1994-12-28 06:14:33 +0000

    Make symbol table extensible, simplify its storage, add a "dump symbols" function.

--- vsta/src/bin/adb/sym.c
+++ vsta/src/bin/adb/sym.c
@@ -15,14 +15,18 @@
 #include <sys/param.h>
 #include "map.h"
 
-static struct nlist *old_syms;		/* a.out information */
-static int num_old_syms;
-static char *old_strtab;
-static int old_strtab_size;
-static struct aout hdr;
+/*
+ * This is all that's really needed from the symbol table
+ */
+struct sym {
+	char *s_name;
+	ulong s_val;
+};
 
-static struct nlist **strtab;		/* After sifting out useless stuff */
-static uint nsym = 0;			/*  ...# left */
+static struct sym *symtab;	/* Name/val mappings */
+static uint nsym = 0;		/*  ...size */
+static struct aout main_hdr;	/* a.out header we loaded first */
+static int hdr_loaded;		/*  ->1 when loaded */
 
 /*
  * rdsym()
@@ -33,7 +37,12 @@ rdsym(char *name)
 {
 	FILE *f;
 	uint i;
+	int strtab_size, num_full_syms;
 	struct nlist *sp;
+	struct aout hdr;
+	char *full_strtab;		/* All entries from all a.out's */
+	struct nlist *full_syms;		/* All nlist entries */
+
 
 	if ((f = fopen(name, "r")) == NULL) {
 		fprintf(stderr, "can't open %s\n", name);
@@ -57,35 +66,41 @@ rdsym(char *name)
 	}
 
 	fseek (f, N_STROFF(hdr), 0);
-	if (fread((char *)&old_strtab_size, sizeof(int), 1, f) != 1) {
+	if (fread((char *)&strtab_size, sizeof(int), 1, f) != 1) {
 		fprintf(stderr, "can't read old strtab size\n");
 		exit(1);
 	}
 
-	if ((old_syms = (struct nlist *)malloc(hdr.a_syms)) == NULL
-	    || ((old_strtab = malloc(old_strtab_size)) == NULL)) {
-		fprintf(stderr, "out of memory\n");
+	full_strtab = malloc(strtab_size);
+	full_syms = malloc(hdr.a_syms);
+	if (!full_syms || !full_strtab) {
+		perror(name);
 		exit(1);
 	}
 
 	fseek(f, N_SYMOFF(hdr), 0);
-	if (fread((char *)old_syms, hdr.a_syms, 1, f) != 1) {
+	if (fread(full_syms, hdr.a_syms, 1, f) != 1) {
 		fprintf (stderr, "can't read symbols\n");
 		exit (1);
 	}
 
 	fseek(f, N_STROFF(hdr), 0);
-	if (fread((char *)old_strtab, old_strtab_size, 1, f) != 1) {
+	if (fread(full_strtab, strtab_size, 1, f) != 1) {
 		fprintf (stderr, "can't read string table\n");
 		exit (1);
 	}
 
-	num_old_syms = hdr.a_syms / sizeof (struct nlist);
+	/*
+	 * All done with the file
+	 */
+	fclose(f);
 
 	/*
-	 * Now index just those symbols which are useful
+	 * Index just those symbols which are useful
 	 */
-	for (i = 0, sp = old_syms; sp && (i < num_old_syms); i++, sp++) {
+	sp = full_syms;
+	num_full_syms = hdr.a_syms / sizeof (struct nlist);
+	for (i = 0; sp && (i < num_full_syms); i++, sp++) {
 		char *name;
 
 		/*
@@ -95,9 +110,9 @@ rdsym(char *name)
 			continue;
 		if (sp->n_un.n_strx == 0)
 			continue;
-		if (sp->n_value >= 0x10000000)
+		if (sp->n_value >= 0x80000000)
 			continue;
-		name = old_strtab + sp->n_un.n_strx;
+		name = full_strtab + sp->n_un.n_strx;
 		if (strlen(name) == 0)
 			continue;
 		if (strchr(name, '.'))
@@ -115,17 +130,33 @@ rdsym(char *name)
 		/*
 		 * Now add to our index
 		 */
-		strtab = realloc(strtab, ++nsym * sizeof(struct nlist *));
-		if (strtab == 0) {
+		symtab = realloc(symtab, ++nsym * sizeof(struct sym));
+		if (symtab == 0) {
 			perror("string table");
 			exit(1);
 		}
-		strtab[nsym-1] = sp;
+		name = strdup(name);
+		if (name == NULL) {
+			perror("string table");
+		}
+		symtab[nsym-1].s_name = name;
+		symtab[nsym-1].s_val = sp->n_value;
 	}
 
-	fclose(f);
-}
+	/*
+	 * Free temp storage
+	 */
+	free(full_strtab);
+	free(full_syms);
 
+	/*
+	 * Snapshot header for first one loaded
+	 */
+	if (!hdr_loaded) {
+		main_hdr = hdr;
+		hdr_loaded = 1;
+	}
+}
 
 /*
  * nameval()
@@ -135,26 +166,26 @@ char *
 nameval(ulong val)
 {
 	uint i;
-	struct nlist *sp;
+	struct sym *s;
 	static char buf[128];
 	ulong closest = 0;
 	char *closename = 0;
 
 	for (i = 0; i < nsym; ++i) {
-		sp = strtab[i];
+		s = &symtab[i];
 
 		/*
 		 * Bound value
 		 */
-		if ((sp->n_value < 0x1000) || (sp->n_value > val))
+		if ((s->s_val < 0x1000) || (s->s_val > val))
 			continue;
 
 		/*
 		 * If it's the closest fit, record it
 		 */
-		if (sp->n_value > closest) {
-			closest = sp->n_value;
-			closename = old_strtab + sp->n_un.n_strx;
+		if (s->s_val > closest) {
+			closest = s->s_val;
+			closename = s->s_name;
 		}
 	}
 
@@ -185,14 +216,12 @@ ulong
 symval(char *p)
 {
 	uint i;
-	struct nlist *sp;
-	char *name;
+	struct sym *s;
 
 	for (i = 0; i < nsym; ++i) {
-		sp = strtab[i];
-		name = old_strtab + sp->n_un.n_strx;
-		if (!strcmp(p, name)) {
-			return(sp->n_value);
+		s = &symtab[i];
+		if (!strcmp(p, s->s_name)) {
+			return(s->s_val);
 		}
 	}
 	return(0);
@@ -206,8 +235,25 @@ map_aout(struct map *m)
 {
 	ulong end_text;
 
-	add_map(m, (void *)0x1000, hdr.a_text + sizeof(hdr), 0);
-	end_text = sizeof(hdr) + 0x1000 + hdr.a_text;
-	add_map(m, (void *)roundup(end_text, 4*1024*1024), hdr.a_data,
-		hdr.a_text + sizeof(struct aout));
+	add_map(m, (void *)0x1000, main_hdr.a_text + sizeof(main_hdr), 0);
+	end_text = sizeof(main_hdr) + 0x1000 + main_hdr.a_text;
+	add_map(m, (void *)roundup(end_text, 4*1024*1024), main_hdr.a_data,
+		main_hdr.a_text + sizeof(struct aout));
+}
+
+/*
+ * dump_syms()
+ *	Dump out symbol table
+ */
+void
+dump_syms(void)
+{
+	uint x;
+	struct sym *s;
+
+	for (x = 0; x < nsym; ++x) {
+		s = &symtab[x];
+		printf("%s=%x\n", s->s_name, s->s_val);
+	}
+	printf("\n");
 }
