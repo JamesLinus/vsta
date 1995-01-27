1000f6490c24ffb42da12f629c110bab8500bae0
vandys <vandys>
1995-01-27 17:09:55 +0000

    Convert to use of path_open(), also allow -n to indicate pure page reclamation (so you can do VM page stealing without swap)

--- vsta/src/bin/cmds/swapd.c
+++ vsta/src/bin/cmds/swapd.c
@@ -21,55 +21,67 @@ main(argc, argv)
 	int argc;
 	char **argv;
 {
-	port_t p;
-	int x;
-
 	/*
-	 * Connect to swap daemon
+	 * Set up for swapping unless -n; for -n, we are only
+	 * launching a thread to do clean page stealing (for
+	 * systems without a swap partition).
 	 */
-	p = msg_connect(PORT_SWAP, ACC_READ|ACC_WRITE);
-	if (p < 0) {
-		perror("swapd can't access daemon");
-		exit(1);
-	}
+	if ((argc < 2) || strcmp(argv[1], "-n")) {
+		port_t p;
+		int x;
 
-	/*
-	 * If there's an initial swap device, hand it to the
-	 * swapper now.
-	 */
-	if (argc > 1) {
-		struct swapadd s;
-		port_name n;
-		struct msg m;
-
-		strcpy(s.s_path, argv[1]);
-		s.s_off = 0;
-		s.s_len = 0;
-
-		m.m_op = SWAP_ADD;
-		m.m_buf = &s;
-		m.m_buflen = sizeof(s);
-		m.m_nseg = 1;
-		m.m_arg = m.m_arg1 = 0;
-		if (msg_send(p, &m) < 0) {
-			perror("swapd initial swap");
+		/*
+		 * Connect to swap daemon
+		 */
+		p = msg_connect(PORT_SWAP, ACC_READ|ACC_WRITE);
+		if (p < 0) {
+			perror("swapd can't access daemon");
 			exit(1);
 		}
+
+		/*
+		 * If there's an initial swap device, hand it to the
+		 * swapper now.
+		 */
+		if (argc > 1) {
+			struct swapadd s;
+			port_name n;
+			struct msg m;
+
+			strcpy(s.s_path, argv[1]);
+			s.s_off = 0;
+			s.s_len = 0;
+
+			m.m_op = SWAP_ADD;
+			m.m_buf = &s;
+			m.m_buflen = sizeof(s);
+			m.m_nseg = 1;
+			m.m_arg = m.m_arg1 = 0;
+			if (msg_send(p, &m) < 0) {
+				perror("swapd initial swap");
+				exit(1);
+			}
+		}
+
+		/*
+		 * Hand off daemon to kernel
+		 */
+		if (set_swapdev(p) < 0) {
+			perror("swapd can't configure swap port");
+			exit(1);
+		}
+		for (x = 0; x < NQIO; ++x) {
+			if (tfork(runqio) < 0) {
+				perror("swapd qio fork");
+			}
+		}
 	}
 
 	/*
-	 * Hand off daemon to kernel
+	 * Launch thread dedicated to scanning memory
 	 */
-	if (set_swapdev(p) < 0) {
-		perror("swapd can't configure swap port");
-		exit(1);
-	}
-	for (x = 0; x < NQIO; ++x) {
-		if (tfork(runqio) < 0) {
-			perror("swapd qio fork");
-		}
-	}
 	pageout();
 	perror("swapd can't start pageout");
+
 	exit(1);
 }
