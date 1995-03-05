bbdd91bd237a3107bf681546fe742addc42f4304
vandys <vandys>
1995-03-05 06:21:31 +0000

    Initial revision

---
+++ vsta/src/bin/cmds/strings.c
@@ -0,0 +1,60 @@
+/*
+ * strings.c
+ *	Extract printable strings from binary stream
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+static int err;
+
+inline static int
+printable(unsigned char c)
+{
+	return(!(c & 0x80) && (isprint(c) || isspace(c)));
+}
+
+static void
+strings(char *file)
+{
+	FILE *fp;
+	int c, inprint = 0, nprint = 0;
+	const int RUNLEN = 4;
+	char print[RUNLEN];
+
+	fp = fopen(file, "rb");
+	if (fp == NULL) {
+		perror(file);
+		err = 1;
+		return;
+	}
+	while ((c = getc(fp)) != EOF) {
+		if (inprint) {
+			if (printable(c)) {
+				putchar(c);
+			} else {
+				inprint = 0;
+			}
+		} else if (printable(c)) {
+			print[nprint++] = c;
+			if (nprint >= RUNLEN) {
+				fwrite(print, 1, RUNLEN, stdout);
+				nprint = 0;
+				inprint = 1;
+			}
+		} else {
+			nprint = 0;
+		}
+	}
+	fclose(fp);
+	putchar('\n');
+}
+
+main(int argc, char **argv)
+{
+	int x;
+
+	for (x = 1; x < argc; ++x) {
+		strings(argv[x]);
+	}
+	return(err);
+}
