f95e98eee1a06848f4816e672d99a00c99fc3f75
vandys <vandys>
1995-04-23 22:01:21 +0000

    Add UID funcs, also some wrappers for POSIX functions which we emulate to the best of our ability to map subset POSIX onto VSTa protections

--- vsta/src/include/unistd.h
+++ vsta/src/include/unistd.h
@@ -8,13 +8,16 @@
  * stuff I'm pretty sure it needs and hope to borrow a manual some
  * time soon.
  */
+#include <sys/types.h>
 
 /*
- * Third argument to lseek (and ftell)
+ * Third argument to lseek (and ftell) - also defined in <stdio.h>
  */
+#if !defined(SEEK_SET) && !defined(SEEK_CUR) && !defined(SEEK_END)
 #define SEEK_SET (0)
 #define SEEK_CUR (1)
 #define SEEK_END (2)
+#endif
 
 /*
  * Second argument to access()
@@ -32,8 +35,21 @@
 #define STDERR_FILENO (2)
 
 /*
- * Function protos
+ * UID/GID related functions
  */
+extern uid_t getuid(void);
+extern uid_t geteuid(void);
+extern gid_t getgid(void);
+extern gid_t getegid(void);
 extern char *getlogin(void);
+
+/*
+ * POSIX things - not really useful to VSTa, but we try to emulate them as
+ * much as possible
+ */
+extern int umask(int);
+extern int chmod(const char *, int);
+extern int access(const char *, int);
+extern int chown(const char *, uid_t, gid_t);
 
 #endif /* _UNISTD_H */
