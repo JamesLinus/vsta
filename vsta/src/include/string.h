b612c5ba43d4c9429aff62f10d5f343e4991d606
vandys <vandys>
1995-04-23 22:00:52 +0000

    Add more string functions, also define NULL

--- vsta/src/include/string.h
+++ vsta/src/include/string.h
@@ -9,28 +9,55 @@
 /*
  * Prototypes
  */
-extern char *strcpy(char *, const char *), *strncpy(char *, const char *, int);
-extern char *strcat(char *, const char *), *strncat(char *, const char *, int);
-extern size_t strlen(const char *);
-extern int strcmp(const char *, const char *),
-	strncmp(const char *, const char *, int);
-extern void *memcpy(void *, const void *, size_t);
-extern char *strchr(const char *, int), *strrchr(const char *, int);
-extern char *index(const char *, int), *rindex(const char *, int);
-extern char *strdup(const char *);
-extern int bcmp(const void *, const void *, unsigned int),
-	memcmp(const void *, const void *, size_t);
-extern void bcopy(const void *, void *, unsigned int);
-extern void bzero(void *, size_t);
-extern char *strtok(char *, const char *),
-	*strpbrk(const char *, const char *),
-	*strstr(const char *, const char *);
-extern size_t strspn(const char *, const char *);
-extern void *memmove(void *, const void *, size_t),
-	*memchr(const void *, unsigned char, size_t),
-	*memset(void *, int, size_t);
-extern char *strsep(char **, const char *);
-extern void swab(const char *, char *, size_t);
-extern size_t strcspn(const char *, const char *);
+extern void bcopy(const void *src, void *dest, unsigned int n);
+extern void bzero(void *s, size_t n);
+extern int bcmp(const void *s1, const void *s2, unsigned int n);
+
+extern void *memcpy(void *dest, const void *src, size_t cnt);
+extern int memcmp(const void *s1, const void *s2, size_t n);
+extern void *memmove(void *dest, const void *src, size_t length);
+extern void *memchr(const void *s, unsigned char c, size_t n);
+extern void *memset(void *dest, int c, size_t n);
+
+extern char *strcpy(char *dest, const char *src);
+extern char *strncpy(char *dest, const char *src, int len);
+extern char *strcat(char *dest, const char *src);
+extern char *strncat(char *dest, const char *src, int len);
+extern size_t strlen(const char *p);
+extern int strcmp(const char *s1, const char *s2);
+extern int strncmp(const char *s1, const char *s2, int nbyte);
+extern int strcoll(const char *a, const char *b);
+extern char *strchr(const char *p, int c);
+extern char *strrchr(const char *p, int c);
+extern char *index(const char *p, int c);
+extern char *rindex(const char *p, int c);
+extern char *strdup(const char *s);
+extern size_t strspn(const char *s1, const char *s2);
+extern size_t strcspn(const char *s1, const char *s2);
+extern char *strpbrk(const char *s1, const char *s2);
+extern char *strstr(const char *s, const char *find);
+extern char *strtok(char *s, const char *delim);
+extern char *strsep(char **stringp, const char *delim);
+extern int strcasecmp(const char *s1, const char *s2);
+extern int strncasecmp(const char *s1, const char *s2, size_t n);
+extern size_t strxfrm(char *s1, const char *s2, size_t n);
+extern void swab(const char *src, char *dest, size_t len);
+
+#define stricmp(s1, s2) strcasecmp(s1, s2)
+#define strnicmp(s1, s2, n) strncasecmp(s1, s2, n)
+
+extern char *strlwr(char *s);
+extern char *strupr(char *s);
+extern char *strrev(char *s);
+extern char *strset(char *s, int ch);
+extern char *strnset(char *s, int ch, size_t n);
+
+/*
+ * NULL defined here for the sake of completeness - I think this is needed
+ * for ANSI C compliance
+ */
+#ifndef NULL
+#define NULL (0)
+#endif
 
 #endif /* _STRING_H */
