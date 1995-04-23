640a13121a235e6ff1d8677b7b406c1fcbe2cea4
vandys <vandys>
1995-04-23 20:53:01 +0000

    Add some <pwd.h> functions

--- vsta/src/lib/pwd.c
+++ vsta/src/lib/pwd.c
@@ -8,8 +8,10 @@
 #include <hash.h>
 #include <std.h>
 #include <string.h>
+#include <paths.h>
 
-static struct hash *uidhash = 0;	/* Mapping GID->struct passwd */
+static struct hash *uidhash = NULL;	/* Mapping UID to struct passwd */
+static FILE *pwfp = NULL;		/* Pointer to passwd file */
 
 /*
  * field()
@@ -37,14 +39,14 @@ field(char *buf, int idx, char *deflt)
 	/*
 	 * Find terminator
 	 */
-	for (q = p+1; *q && (*q != ':'); ++q)
+	for (q = p + 1; *q && (*q != ':'); ++q)
 		;
 
 	/*
 	 * Calculate length, get static string to hold value
 	 */
-	len = q-p;
-	fldval = realloc(fldval, len+1);
+	len = q - p;
+	fldval = realloc(fldval, len + 1);
 	if (fldval == 0) {
 		return(deflt);
 	}
@@ -58,69 +60,91 @@ field(char *buf, int idx, char *deflt)
 }
 
 /*
+ * read_pfile()
+ *	Read the next passwd entry from a specified file
+ *
+ * We take a pointer to the file to be interrogated and read the next entry
+ * into a passwd structure.  If there are no more entries or there's an error
+ * we return NULL
+ */
+static struct passwd *
+read_pwfile(FILE *fp, struct passwd *pw)
+{
+	char buf[256], *p;
+
+	if (!fgets(buf, sizeof(buf)-1, fp)) {
+		return(NULL);
+	}
+
+	/*
+	 * Trim newline
+	 */
+	buf[strlen(buf)-1] = '\0';
+
+	/*
+	 * Fill in
+	 */
+	pw->pw_name = strdup(field(buf, 0, "???"));
+	pw->pw_uid = atoi(field(buf, 2, "2"));
+	pw->pw_gid = atoi(field(buf, 3, "2"));
+	pw->pw_dir = strdup(field(buf, 6, "/vsta"));
+	pw->pw_shell = strdup(field(buf, 8, _PATH_DEFAULT_SHELL));
+
+	return(pw);
+}
+
+/*
  * fill_hash()
- *	One-time read of passwd file into local cache
+ *	One-time read of password file into local cache
  */
 static void
 fill_hash(void)
 {
 	FILE *fp;
-	char buf[256], *p;
 	struct passwd *pw;
+	int all_done = 0;
 
 	/*
-	 * Access passwd file
+	 * Access password file
 	 */
-	if ((fp = fopen("/vsta/etc/passwd", "r")) == 0) {
+	if ((fp = fopen(_PATH_PASSWD, "r")) == 0) {
 		return;
 	}
 
 	/*
 	 * Allocate hash
 	 */
-	uidhash = hash_alloc(16);
-	if (uidhash == 0) {
+	if ((uidhash = hash_alloc(16)) == NULL) {
 		abort();
-	}
+	}	
 
 	/*
 	 * Read lines
 	 */
-	buf[sizeof(buf)-1] = '\0';
-	while (fgets(buf, sizeof(buf)-1, fp)) {
+	while (!all_done) {
 		/*
-		 * Trim newline
+		 * Allocate new passwd structure
 		 */
-		buf[strlen(buf)-1] = '\0';
-
-		/*
-		 * Allocate new passwd
-		 */
-		pw = malloc(sizeof(struct passwd));
-		if (pw == 0) {
+		if ((pw = malloc(sizeof(struct passwd))) == NULL) {
+			all_done = 1;
 			break;
 		}
 
-		/*
-		 * Fill in
-		 */
-		pw->pw_name = strdup(field(buf, 0, "???"));
-		pw->pw_uid = atoi(field(buf, 2, "2"));
-		pw->pw_gid = atoi(field(buf, 3, "2"));
-		pw->pw_dir = strdup(field(buf, 6, "/vsta"));
-		pw->pw_shell = strdup(field(buf, 8, "/vsta/bin/testsh"));
+		if (read_pwfile(fp, pw) == NULL) {
+			all_done = 1;
+			break;
+		}
 
 		/*
 		 * Add to hash
 		 */
 		if (hash_insert(uidhash, pw->pw_uid, pw)) {
 			/*
-			 * XXX we lose the strdup()'ed fields, but no
+			 * XXX we lose the strdup()'d fields, but there's no
 			 * easy way to tell if they were the default
-			 * values or strdup()'ed.
+			 * values or the strdup()'d ones
 			 */
 			free(pw);
-			break;
 		}
 	}
 	fclose(fp);
@@ -158,7 +182,7 @@ struct hasharg {
  * namecheck()
  *	Check for match on name, end foreach when find it
  */
-static
+static int
 namecheck(uid_t uid, struct passwd *pw, struct hasharg *ha)
 {
 	if (!strcmp(pw->pw_name, ha->name)) {
@@ -170,7 +194,7 @@ namecheck(uid_t uid, struct passwd *pw, struct hasharg *ha)
 
 /*
  * getpwnam()
- *	Get struct passwd given passwd name
+ *	Get struct passwd given a user name
  */
 struct passwd *
 getpwnam(char *name)
@@ -190,6 +214,84 @@ getpwnam(char *name)
 }
 
 /*
+ * getpwent()
+ *	Get next passwd file entry
+ */
+struct passwd *
+getpwent(void)
+{
+	static struct passwd *pwd = NULL;
+
+	if (pwfp == NULL) {
+		if ((pwfp = fopen(_PATH_PASSWD, "r")) == NULL) {
+			return(NULL);
+		}
+	}
+	if (pwd == NULL) {
+		if ((pwd = (struct passwd *)malloc(sizeof(struct passwd)))
+				== NULL) {
+			return(NULL);
+		}
+	}
+
+	/*
+	 * Now read the next entry
+	 */
+	return(read_pwfile(pwfp, pwd));
+}
+
+/*
+ * fgetpwent()
+ *	Get the next passwd file entry from the specified file
+ */
+struct passwd *
+fgetpwent(FILE *stream)
+{
+	static struct passwd *pwd = NULL;
+
+	if (stream == NULL) {
+		return(NULL);
+	}
+	if (pwd == NULL) {
+		if ((pwd = (struct passwd *)malloc(sizeof(struct passwd)))
+				== NULL) {
+			return(NULL);
+		}
+	}
+
+	/*
+	 * Now read the next entry
+	 */
+	return(read_pwfile(stream, pwd));
+}
+
+/*
+ * setpwent()
+ *	Make sure the next getpwent() references the first entry of the
+ *	passwd file
+ */
+void
+setpwent(void)
+{
+	if (pwfp) {
+		rewind(pwfp);
+	}
+}
+
+/*
+ * endpwent()
+ *	Close access to the passwd file
+ */
+void
+endpwent(void)
+{
+	if (pwfp) {
+		fclose(pwfp);
+		pwfp = NULL;
+	}
+}
+
+/*
  * getuid()
  *	Get UID from first permission record
  */
@@ -202,6 +304,16 @@ getuid(void)
 		return(0);
 	}
 	return(me.perm_uid);
+}
+
+/*
+ * geteuid()
+ *	Get effective UID - we don't have these of course!
+ */
+uid_t
+geteuid(void)
+{
+	return(getuid());
 }
 
 /*
@@ -234,4 +346,14 @@ getgid(void)
 		return(0);
 	}
 	return(pw->pw_gid);
+}
+
+/*
+ * getegid()
+ *	Get effective GID - we don't have these either
+ */
+gid_t
+getegid(void)
+{
+	return(getgid());
 }
