c82115dc2f4b87130bab975d0b45a17ecc091cc2
vandys <vandys>
1995-04-24 03:05:34 +0000

    Unused limit param

--- vsta/src/include/sys/param.h
+++ vsta/src/include/sys/param.h
@@ -19,7 +19,6 @@
 #define ERRLEN 16	/*  ...in error string */
 #define MSGSEGS 4	/* Max # segments to a message */
 			/* Does not include m_seg0, so add one for kernel */
-#define NPROC 64	/* Number of threads on system */
 #define KSTACK_SIZE \
 	(NBPG)		/* Size of kernel stack */
 #define MAX_WIRED (32)	/* Max # pages wired for DMA at once */
