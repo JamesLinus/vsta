8a2d624381aab62f35e63782624a1b75f3a6d8d6
vandys <vandys>
1995-08-13 17:41:48 +0000

    Make it work

--- vsta/src/bin/cmds/rcsdo.c
+++ vsta/src/bin/cmds/rcsdo.c
@@ -6,38 +6,34 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-static char cmd[128], buf[128];
-static char curdir[256];
-static int verbose = 0;
+static char cmd[256], buf[256];
+static char curdir[512];
 
 static void
-do_dir(DIR *d, int in_rcs)
+do_dir(DIR *d)
 {
 	long pos;
 	int olen;
 	struct dirent *de;
 	struct stat sb;
+	char *nm;
 
-	if (verbose) {
-		printf("Enter %sdir %s\n",
-			in_rcs ? "RCS " : "", curdir);
-	}
 	olen = strlen(curdir);
 	while (de = readdir(d)) {
+
 		/*
-		 * Skip ".", ".."
+		 * Skip names starting in ".", and "rcs" and "RCS"
 		 */
-		if (de->d_name[0] == '.') {
-			if (!strcmp(de->d_name, ".") ||
-					!strcmp(de->d_name, "..")) {
-				continue;
-			}
+		nm = de->d_name;
+		if (!strcmp(nm, "rcs") || !strcmp(nm, "RCS") ||
+				(de->d_name[0] == '.')) {
+			continue;
 		}
 
 		/*
 		 * Get dope on entry
 		 */
-		sprintf(buf, "%s/%s", curdir, de->d_name);
+		sprintf(buf, "%s/%s", curdir, nm);
 		if (stat(buf, &sb) < 0) {
 			continue;
 		}
@@ -48,22 +44,13 @@ do_dir(DIR *d, int in_rcs)
 		 * close the current dir during the recursion.
 		 */
 		if ((sb.st_mode & S_IFMT) == S_IFDIR) {
-			/*
-			 * Sanity
-			 */
-			if (in_rcs) {
-				printf("Error: dir in RCS/: %s\n",
-					de->d_name);
-				continue;
-			}
-
 			pos = telldir(d);
 			closedir(d);
-			sprintf(curdir+olen, "/%s", de->d_name);
+			sprintf(curdir+olen, "/%s", nm);
 			if (d = opendir(curdir)) {
-				do_dir(d, !strcmp(de->d_name, "rcs"));
+				do_dir(d);
 			} else {
-				perror(de->d_name);
+				perror(nm);
 			}
 			closedir(d);
 			curdir[olen] = '\0';
@@ -73,19 +60,17 @@ do_dir(DIR *d, int in_rcs)
 		}
 
 		/*
-		 * If not an RCS directory, ignore
+		 * Make sure it's an RCS file
 		 */
-		if (!in_rcs) {
+		sprintf(buf, "%s/RCS/%s", curdir, nm);
+		if (access(buf, 0) < 0) {
 			continue;
 		}
 
 		/*
 		 * Otherwise apply the command to this file
 		 */
-		sprintf(buf, "%s%s", cmd, de->d_name);
-		if (verbose) {
-			printf(" - %s\n", buf);
-		}
+		sprintf(buf, "%s %s/%s", cmd, curdir, nm);
 		system(buf);
 	}
 }
@@ -95,11 +80,6 @@ main(int argc, char **argv)
 	DIR *d;
 	int x;
 
-	if ((argc > 1) && !strcmp(argv[1], "-v")) {
-		argc -= 1;
-		argv += 1;
-		verbose = 1;
-	}
 	if (argc < 2) {
 		printf("Usage is: %s [-v] <RCS commands...>\n", argv[0]);
 		exit(1);
@@ -114,7 +94,7 @@ main(int argc, char **argv)
 		perror(".");
 		exit(1);
 	}
-	do_dir(d, 0);
+	do_dir(d);
 	closedir(d);
 	return(0);
 }
