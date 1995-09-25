c4251033676fce96394825dbb8a8c17e457bd3db
vandys <vandys>
1995-09-25 01:41:38 +0000

    Add shadow password support, plus a standard path for login

--- vsta/src/include/paths.h
+++ vsta/src/include/paths.h
@@ -7,9 +7,11 @@
 #define _PATH_ENV "/env"
 #define _PATH_NAMER "/namer"
 #define _PATH_PASSWD "/vsta/etc/passwd"
+#define _PATH_SHADOW "/vsta/etc/shadow"
 #define _PATH_GROUP "/vsta/etc/group"
 #define _PATH_IDS "/vsta/etc/ids"
 #define _PATH_DEFAULT_SHELL "/vsta/bin/sh"
 #define _PATH_TERMCAP "/vsta/lib/termcap"
+#define _PATH_LOGIN "/vsta/bin/login"
 
 #endif /* _PATHS_H */
