e9c78b73a4c744dac0b7287acc4a968c6b2dd802
vandys <vandys>
1995-10-12 22:02:44 +0000

    Permit GCC users to use the inline flavor instead of procedure calls

--- vsta/src/include/mach/io.h
+++ vsta/src/include/mach/io.h
@@ -7,13 +7,76 @@
 
 #include <sys/types.h>
  
-/*
- * Prototypes for a few short I/O routines in the C library
- */
+#ifndef __GNUC__
 extern uchar inportb(int port);
 extern void outportb(int port, uchar data);
-extern ushort inportw();
+extern ushort inportw(int port);
 extern void outportw(int port, ushort data);
+#else
+/*
+ * On GNU C, we can inline assembly functions, with a significant
+ * gain in speed and modest reduction in code size.
+ */
+
+/*
+ * inportb()
+ *	Get a byte from an I/O port
+ */
+inline static uchar
+inportb(int port)
+{
+	register uchar res;
+	
+	__asm__ __volatile__(
+		"inb %%dx,%%al\n\t"
+		: "=a" (res)
+		: "d" (port));
+	return(res);
+}
+
+/*
+ * outportb()
+ *	Write a byte to an I/O port
+ */
+inline static void
+outportb(int port, uchar data)
+{
+	__asm__ __volatile__(
+		"outb %%al,%%dx\n\t"
+		: /* No output */
+		: "a" (data), "d" (port));
+}
+
+/*
+ * inportw()
+ *	Get a word from an I/O port
+ */
+inline static ushort
+inportw(int port)
+{
+	register ushort res;
+	
+	__asm__ __volatile__(
+		"inw %%dx,%%ax\n\t"
+		: "=a" (res)
+		: "d" (port));
+	return(res);
+}
+
+/*
+ * outportw()
+ *	Write a word to an I/O port
+ */
+inline static void
+outportw(int port, ushort data)
+{
+	__asm__ __volatile__(
+		"outw %%ax,%%dx\n\t"
+		: /* No output */
+		: "a" (data), "d" (port));
+}
+#endif
+
 extern void repinsw(int port, void *buffer, int count);
 extern void repoutsw(int port, void *buffer, int count);
 
