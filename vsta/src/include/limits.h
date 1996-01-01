d4c0a3df837a26a6552a21d97409a67c5d4a7e54
vandys <vandys>
1996-01-01 16:25:39 +0000

    Add path max, protect against multiple includes

--- vsta/src/include/limits.h
+++ vsta/src/include/limits.h
@@ -1,1 +1,18 @@
+#ifndef _LIMITS_H
+#define _LIMITS_H
+/*
+ * limits.h
+ *	Machine independent OS limits
+ */
+
+/*
+ * Max path length
+ */
+#define _POSIX_PATH_MAX (256)
+
+/*
+ * Nest in machine specific ones now
+ */
 #include <mach/limits.h>
+
+#endif /* _LIMITS_H */
