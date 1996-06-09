7e6e38691ddf3afbb92f389df6b3e8efe840a9be
vandys <vandys>
1996-06-09 06:24:42 +0000

    Add -p

--- vsta/src/bin/cmds/mkdir.c
+++ vsta/src/bin/cmds/mkdir.c
@@ -2,15 +2,76 @@
  * mkdir.c
  *	Create directories
  */
+#include <std.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+static int pflag, errs;
+
+/*
+ * do_mkdir()
+ *	mkdir() action, perhaps creating leading path elements
+ */
+static void
+do_mkdir(char *p)
+{
+	char *q;
+
+	if (mkdir(p) >= 0) {
+		return;
+	}
+	if (!pflag) {
+		perror(p);
+		errs = 1;
+		return;
+	}
+	q = p;
+	for (;;) {
+		char *buf;
+
+		q = strchr(q, '/');
+		if (q) {
+			int len;
+
+			len = q - p;
+			buf = malloc(len + 1);
+			bcopy(p, buf, len);
+			buf[len] = '\0';
+		} else {
+			buf = p;
+		}
+		if ((mkdir(buf) < 0) && (errno != EEXIST)) {
+			perror(buf);
+			if (q) {
+				free(buf);
+			}
+			errs = 1;
+			return;
+		}
+		if (!q) {
+			return;
+		}
+		free(buf);
+		q += 1;
+	}
+}
+
+int
 main(int argc, char **argv)
 {
-	int x, errs = 0;
+	int x;
 	
+	/*
+	 * -p: create path elements
+	 */
+	if (!strcmp(argv[1], "-p")) {
+		++argv;
+		--argc;
+		pflag = 1;
+	}
 	for (x = 1; x < argc; ++x) {
-		if (mkdir(argv[x]) < 0) {
-			perror(argv[x]);
-			errs++;
-		}
+		do_mkdir(argv[x]);
 	}
 	return(errs);
 }
