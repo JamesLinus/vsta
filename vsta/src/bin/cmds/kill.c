14cef40aa54513a616c137f2ccfe293530ec82e9
vandys <vandys>
1996-12-08 16:12:52 +0000

    Add process group events as an option

--- vsta/src/bin/cmds/kill.c
+++ vsta/src/bin/cmds/kill.c
@@ -2,15 +2,19 @@
 
 main(int argc, char **argv)
 {
-	int x, pid;
+	int x, pid, pgrp = 0;
 	char *event = EKILL;
 
 	for (x = 1; x < argc; ++x) {
 		if (argv[x][0] == '-') {
-			event = argv[x]+1;
+			if (!strcmp(argv[x], "-p")) {
+				pgrp = 1;
+			} else {
+				event = argv[x]+1;
+			}
 		} else {
 			pid = atoi(argv[x]);
-			notify(pid, 0, event);
+			notify(pid, pgrp ? -1 : 0, event);
 		}
 	}
 	return(0);
