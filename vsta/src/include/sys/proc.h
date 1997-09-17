a8205be726ef29f16e0d144d9fd32521d1f3fab4
vandys <vandys>
1997-09-17 12:28:56 +0000

    Create sys/ptrace.h

--- vsta/src/include/sys/proc.h
+++ vsta/src/include/sys/proc.h
@@ -45,29 +45,11 @@ struct pdbg {
 	uint pd_flags;		/* See below */
 };
 
-/* Bits in pd_flags */
-#define PD_ALWAYS 1		/* Stop at next chance */
-#define PD_SYSCALL 2		/*  ...before & after syscalls */
-#define PD_EVENT 4		/*  ...when receiving an event */
-#define PD_EXIT 8		/*  ...when exiting */
-#define PD_BPOINT 16		/*  ...breakpoint reached */
-#define PD_EXEC 32		/*  ...exec done (new addr space) */
-#define PD_CONNECTING 0x8000	/* Slave has connect in progress */
-
 /*
- * Values for m_op of a debug message.  The sense of the operation
- * is further defined by m_arg and m_arg1.
+ * We have this in its own file so debuggers don't have to hear
+ * about all our internal data structures.
  */
-#define PD_SLAVE 300		/* Slave ready for commands */
-#define PD_RUN 301		/* Run */
-#define PD_STEP 302		/* Run one instruction, then break */
-#define PD_BREAK 303		/* Set/clear breakpoint */
-#define PD_RDREG 304		/* Read registers */
-#define PD_WRREG 305		/*  ...write */
-#define PD_MASK 306		/* Set mask */
-#define PD_RDMEM 307		/* Read memory */
-#define PD_WRMEM 308		/*  ...write */
-#define PD_MEVENT 309		/* Read/write event */
+#include <sys/ptrace.h>
 
 /*
  * Handy macro for checking if we should drop into debug slave
