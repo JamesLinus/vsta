71d2f44f4e249d0799344bedc914901e63579d59
vandys <vandys>
1998-12-13 22:19:33 +0000

    More bug fixing and bringup of page wiring (and ISA bounce buffers)

--- vsta/src/os/mach/vm.c
+++ vsta/src/os/mach/vm.c
@@ -32,7 +32,7 @@ check_low(uint pfn)
  */
 char *
 mach_page_wire(uint flags, struct pview *pv,
-	struct perpage *pp, void *va)
+	struct perpage *pp, void *va, uint idx)
 {
 	uint newpfn;
 
@@ -41,38 +41,60 @@ mach_page_wire(uint flags, struct pview *pv,
 	/*
 	 * Looking for an ISA DMA-able page?
 	 */
-	if (flags & WIRE_16M) {
+	if (!(flags & WIRE_16M)) {
 		/*
-		 * No problem.
+		 * Other mach bits are unknown; ignore
 		 */
-		if (pp->pp_pfn < btop(LIM)) {
-			return(0);
-		}
-
-		/*
-		 * Try to grab a low page
-		 */
-		if (alloc_page_fn(check_low, &newpfn)) {
-			return(ENOMEM);
-		}
-
-		/*
-		 * Duplicate the page contents
-		 */
-		bcopy(ptov(ptob(pp->pp_pfn)), ptov(ptob(newpfn)), NBPG);
-
-		/*
-		 * Remove any old translation,
-		 * free the old page, and use this one
-		 */
-		hat_deletetrans(pv, va, pp->pp_pfn);
-		free_page(pp->pp_pfn);
-		pp->pp_pfn = newpfn;
 		return(0);
 	}
 
 	/*
-	 * Other mach bits are unknown; ignore
+	 * No problem.
 	 */
+	if (pp->pp_pfn < btop(LIM)) {
+		return(0);
+	}
+
+	/*
+	 * Try to grab a low page
+	 */
+	if (alloc_page_fn(check_low, &newpfn)) {
+		return(ENOMEM);
+	}
+
+	/*
+	 * Duplicate the page contents
+	 */
+	bcopy(ptov(ptob(pp->pp_pfn)), ptov(ptob(newpfn)), NBPG);
+
+	/*
+	 * Remove any old translations
+	 */
+	pp->pp_flags |= vm_unvirt(pp);
+	if (pp->pp_atl) {
+		/*
+		 * A cache reference is acceptable, as it doesn't
+		 * correspond to anybody who has a mapping to the
+		 * actual page.
+		 */
+		ASSERT_DEBUG(pp->pp_refs > 0, "mach_page_wire: atl !ref");
+		if (!(pp->pp_atl->a_flags & ATL_CACHE)) {
+			return(EBUSY);
+		}
+	} else {
+		/*
+		 * Insert a cache reference in case we've removed the
+		 * last "real" reference to the page.
+		 */
+		ASSERT_DEBUG(pp->pp_refs == 0, "mach_page_wire: !atl ref");
+		pp->pp_refs = 1;
+		add_atl(pp, pv->p_set, idx, ATL_CACHE);
+	}
+
+	/*
+	 * Free the old page, and use this one
+	 */
+	free_page(pp->pp_pfn);
+	pp->pp_pfn = newpfn;
 	return(0);
 }
