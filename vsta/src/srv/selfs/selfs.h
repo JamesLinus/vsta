0e091821116e0e9ff730b12ca6bcf0795f0fe9a4
vandys <vandys>
2000-03-08 17:01:18 +0000

    Move common defs out to /vsta/include/select.h

--- vsta/src/srv/selfs/selfs.h
+++ vsta/src/srv/selfs/selfs.h
@@ -79,40 +79,9 @@ extern void selfs_open(struct msg *, struct file *),
 extern struct hash *filehash;
 
 /*
- * Format of messages used to control selfs
+ * Data shared with library side of implementation
  */
-
-/*
- * This is the format of data returned from an FS_READ to a client.  It
- * indicates which sources satisfied the select request.  Zero (for timeout)
- * or more of these will be returned as the FS_READ data.
- */
-struct select_complete {
-	uint sc_index;		/* Index of client connection */
-	uint sc_mask;		/* Mask of satisfied events */
-	ulong sc_iocount;	/* # message exchanges seen */
-};
-
-/*
- * A server will write one or more of these to selfs to indicate
- * events which should awake a client.
- */
-struct select_event {
-	long se_clid;		/* Client's connection ID */
-	ulong se_key;		/* Magic # to inhibit bogus clid's */
-	uint se_index;		/* Index of client's server connection */
-	uint se_mask;		/* Mask of events which have occurred */
-	ulong se_iocount;	/* Indicated sc_iocount for this event */
-};
-
-/*
- * While not a struct, the format of the data which a client writes to a
- * server in an FS_WSTAT message is:
- *  select=<mask>,<host>,<se_clid>,<se_index>,<key>
- * or:
- *  unselect=<mask>,<host>,<se_clid>,<se_index>,<key>
- * The server's iocount is expected to count up from 0 starting from this
- * point.
- */
+#define _SELFS_INTERNAL
+#include <select.h>
 
 #endif /* SELFS_H */
