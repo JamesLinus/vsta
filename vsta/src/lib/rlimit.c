0a19e69f386886b009c785af2b55d7fc9d7eff6a
vandys <vandys>
2000-04-05 21:45:25 +0000

    Add nice() and clock() API's. Support process time fields of getrusage().

--- vsta/src/lib/rlimit.c
+++ vsta/src/lib/rlimit.c
@@ -5,7 +5,9 @@
  * XXX for now, just a stub
  */
 #include <resource.h>
-#include <sys/fs.h>
+#include <sys/fs.h>		/* For error names */
+#include <sys/pstat.h>
+#include <time.h>
 
 int
 getpriority(int which, int who)
@@ -31,7 +33,30 @@ getrlimit(int which, struct rlimit *r)
 int
 getrusage(int which, struct rusage *r)
 {
+	struct pstat_proc ps;
+
+	/*
+	 * Most fields are not supported
+	 */
 	bzero(r, sizeof(*r));
+
+	/*
+	 * Pull our user and system CPU times from pstat()
+	 */
+	if (pstat(PSTAT_PROC, getpid(), &ps, sizeof(ps)) < 0) {
+		return(-1);
+	}
+
+	/*
+	 * Adapt units
+	 * TBD: our CPU versus children's.  Probably going to have
+	 *  to change kernel accounting, and then the pstat API.
+	 */
+	r->ru_utime.tv_sec = ps.psp_usrcpu / HZ;
+	r->ru_utime.tv_usec = (ps.psp_usrcpu % HZ) * (1000000 / HZ);
+	r->ru_stime.tv_sec = ps.psp_syscpu / HZ;
+	r->ru_stime.tv_usec = (ps.psp_syscpu % HZ) * (1000000 / HZ);
+
 	return(0);
 }
 
@@ -46,6 +71,16 @@ setpriority(int which, int who, int prio)
 }
 
 /*
+ * nice()
+ *	Adjust priority relative to current value
+ */
+int
+nice(int incr)
+{
+	return(__seterr(ENOTSUP));
+}
+
+/*
  * setrlimit()
  *	Control resource consumption of a process
  */
@@ -53,4 +88,23 @@ int
 setrlimit(int which, const struct rlimit *r)
 {
 	return(__seterr(ENOTSUP));
+}
+
+/*
+ * clock()
+ *	Tell amount of CPU time since program started
+ */
+clock_t
+clock(void)
+{
+	struct rusage r;
+
+	/*
+	 * Extract our CPU usage
+	 */
+	if (getrusage(RUSAGE_SELF, &r) < 0) {
+		return(-1);
+	}
+	return ((r.ru_utime.tv_sec * CLOCKS_PER_SEC) +
+		((r.ru_utime.tv_usec * CLOCKS_PER_SEC) / 1000000));
 }
