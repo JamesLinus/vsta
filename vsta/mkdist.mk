d8080453637657fd86f63b36562c3e9499b932b3
vandys <vandys>
2000-05-21 15:09:51 +0000

    Need to provide include source!

--- vsta/mkdist.mk
+++ vsta/mkdist.mk
@@ -27,7 +27,7 @@ SRCSRV=src/srv/bfs src/srv/cdfs src/srv/devnull src/srv/dos \
 # Core source distribution
 SRC=$(LAW) mkdist.mk rcs \
 	src/bin/adb src/bin/init src/bin/login src/bin/roff \
-	src/bin/cmds src/bin/time src/lib src/os \
+	src/bin/cmds src/bin/time src/include src/lib src/os \
 	$(SRCSRV) src/boot.386
 
 # Networking
