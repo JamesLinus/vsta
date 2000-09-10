cd2cb4e9134352e87352984c454eb5e2e424a04d
vandys <vandys>
2000-09-10 15:40:38 +0000

    Fix day 1 bug when running out of slots. Add rmap_grab() to pick slots to take. Factor out common code.

--- vsta/src/lib/rmap.c
+++ vsta/src/lib/rmap.c
@@ -24,8 +24,27 @@ ulong lost_elems = 0L;	/* Diagnostic for space lost due to fragmentation */
 void
 rmap_init(struct rmap *r, uint size)
 {
+	/*
+	 * Record # slots available, which is one less (since we use the
+	 * first as our own header.
+	 */
 	r->r_off = 0;
-	r->r_size = size;
+	r->r_size = size-1;
+}
+
+/*
+ * collapse()
+ *	An entry has been emptied, so make it disappear
+ */
+static void
+collapse(struct rmap *rmap, struct rmap *r)
+{
+	struct rmap *rlim = &rmap[rmap->r_off];
+
+	rmap->r_off -= 1;
+	if (r < rlim) {
+		bcopy(r+1, r, sizeof(struct rmap)*(rlim-r));
+	}
 }
 
 /*
@@ -50,8 +69,9 @@ rmap_alloc(struct rmap *rmap, uint size)
 		if (r->r_size >= size)
 			break;
 	}
-	if (r > rlim)
+	if (r > rlim) {
 		return(0);
+	}
 
 	/*
 	 * Trim the resource element if it still has some left,
@@ -62,12 +82,43 @@ rmap_alloc(struct rmap *rmap, uint size)
 		r->r_off += size;
 		r->r_size -= size;
 	} else {
-		rmap->r_off -= 1;
-		if (r < rlim) {
-			bcopy(r+1, r, sizeof(struct rmap)*(rlim-r));
-		}
+		collapse(rmap, r);
 	}
 	return(idx);
+}
+
+/*
+ * makespace()
+ *	Insert room for a new slot at the given position
+ *
+ * Returns 1 if there isn't room to insert the element, 0 on success.
+ */
+static int
+makespace(struct rmap *rmap, struct rmap *r)
+{
+	struct rmap *rlim = &rmap[rmap->r_off];
+
+	/*
+	 * If no room to insert slot, return failure
+	 */
+	if (rmap->r_size == rmap->r_off) {
+		return(1);
+	}
+	rmap->r_off += 1;
+
+	/*
+	 * If inserting in middle, slide up entries
+	 */
+	if (r <= rlim) {
+		bcopy(r, r+1, sizeof(struct rmap) * ((rlim-r)+1));
+		return(0);
+	}
+
+	/*
+	 * Otherwise it's added at the end
+	 */
+	ASSERT_DEBUG(r == rlim+1, "rmap makespace: invalid insert");
+	return(0);
 }
 
 /*
@@ -129,31 +180,131 @@ rmap_free(struct rmap *rmap, uint off, uint size)
 	/*
 	 * Need to add a new element.  See if it'll fit.
 	 */
-	if (rmap->r_size == rmap->r_off) {
+	if (makespace(rmap, r)) {
 		/*
 		 * Nope.  Tabulate and lose the space.
 		 */
 		lost_elems += size;
 		return;
 	}
-	rmap->r_off += 1;
 
 	/*
-	 * If we found the place to insert, do so
+	 * Record it
 	 */
-	if (r <= rlim) {
-		bcopy(r, r+1, sizeof(struct rmap) * ((rlim-r)+1));
-		r->r_off = off;
-		r->r_size = size;
-		return;
+	r->r_off = off;
+	r->r_size = size;
+}
+
+/*
+ * rmap_grab()
+ *	Grab the requested range, or return failure if any of it is not free
+ *
+ * Returns 0 on success, 1 on failure.
+ */
+int
+rmap_grab(struct rmap *rmap, uint off, uint size)
+{
+	struct rmap *r, *rlim;
+	uint x, top, rtop;
+
+	rlim = &rmap[rmap->r_off];
+	for (r = &rmap[1]; r <= rlim; ++r) {
+		/*
+		 * If we've advanced beyond the requested offset,
+		 * we can never match, so end the loop.
+		 */
+		if (r->r_off > off) {
+			break;
+		}
+
+		/*
+		 * See if this is the range which will hold our request
+		 */
+		top = r->r_off + r->r_size;
+		if (!((r->r_off <= off) && (top > off))) {
+			continue;
+		}
+
+		/*
+		 * Since this run encompasses the requested range, we
+		 * can either grab all of it, or none of it.
+		 */
+
+		/*
+		 * The top of our request extends beyond the block; fail.
+		 */
+		rtop = off + size;
+		if (rtop > top) {
+			return(1);
+		}
+
+		/*
+		 * If the requested start matches our range's start, we
+		 * can simply shave it off the front.
+		 */
+		if (off == r->r_off) {
+			r->r_off += size;
+			r->r_size -= size;
+			if (r->r_size == 0) {
+				collapse(rmap, r);
+			}
+			return(0);
+		}
+
+		/*
+		 * Similarly, if the end matches, we can shave the end
+		 */
+		if (rtop == top) {
+			r->r_size -= size;
+
+			/*
+			 * We know r_size > 0, since otherwise we would've
+			 * matched the previous case otherwise.
+			 */
+			ASSERT_DEBUG(r->r_size > 0, "phase error");
+
+			return(0);
+		}
+
+		/*
+		 * Otherwise we must split the range
+		 */
+		if (makespace(rmap, r)) {
+			uint osize;
+
+			/*
+			 * No room for further fragmentation, so chop off
+			 * to the tail.
+			 */
+			osize = r->r_size;
+			r->r_size = top - rtop;
+			lost_elems += (osize - r->r_size) - size;
+			r->r_off = rtop;
+			return(0);
+		}
+
+		/*
+		 * The current slot is everything below us
+		 */
+		r->r_size = off - r->r_off;
+
+		/*
+		 * The new slot is everything above
+		 */
+		++r;
+		r->r_off = rtop;
+		r->r_size = top - rtop;
+
+		/*
+		 * OK
+		 */
+		return(0);
 	}
 
 	/*
-	 * Otherwise add it at the end
+	 * Nope, not in our range of free entries
 	 */
-	++rlim;
-	rlim->r_off = off;
-	rlim->r_size = size;
+	return(1);
 }
 
 #ifdef DEBUG
