86d7cd7a6f7a258d1cb354728cb108ec7429a2c6
vandys <vandys>
2001-01-04 16:14:54 +0000

    Some more compatibility defines

--- vsta/src/include/cdefs.h
+++ vsta/src/include/cdefs.h
@@ -5,6 +5,9 @@
 #ifndef _CDEFS_H
 #define _CDEFS_H
 
+#define __BEGIN_DECLS		/* VSTa does not support C++ */
+#define __END_DECLS
 #define __P(protos) protos	/* VSTa only supports ANSI C */
+#define __unused
 
 #endif /* _CDEFS_H */
