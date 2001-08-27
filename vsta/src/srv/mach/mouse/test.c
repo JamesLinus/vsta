66b125b9f269a5b4ca58a9d9e71efc7c917e5eb7
vandys <vandys>
2001-08-27 14:52:41 +0000

    Add select() support testing.

--- vsta/src/srv/mach/mouse/test.c
+++ vsta/src/srv/mach/mouse/test.c
@@ -1,17 +1,78 @@
 #include <sys/fs.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <select.h>
 
-int 
-main(void)
+/*
+ * readit()
+ *	Read mouse status, and display on stdout
+ */
+static void
+readit(int mouse)
 {
-	port_t mouse;
-	struct msg m;
 	char *statstr;
 
-	mouse = path_open("srv/mouse", ACC_READ);
-	if (mouse < 0) {
-		perror("srv/mouse");
+	statstr = fd_rstat(mouse, NULL);
+	printf("%d %d %d %3d %3d\n",
+		atoi(__fieldval(statstr, "left")),
+		atoi(__fieldval(statstr, "middle")),
+		atoi(__fieldval(statstr, "right")),
+		atoi(__fieldval(statstr, "dx")),
+		atoi(__fieldval(statstr, "dy")));
+}
+
+/*
+ * test_select()
+ *	Test operation of select()'ing for events
+ */
+static void
+test_select(int mousefd)
+{
+	fd_set s;
+	int ret;
+	struct timeval to;
+
+	for (;;) {
+		FD_ZERO(&s); FD_SET(mousefd, &s);
+		to.tv_sec = 2;
+		to.tv_usec = 0;
+		ret = select(mousefd+1, &s, NULL, NULL, &to);
+		if (ret > 0) {
+			printf("Mouse event\n");
+			readit(mousefd);
+			continue;
+		}
+		if (ret == 0) {
+			printf("Timeout\n");
+		}
+		perror("select");
+		sleep(1);
+	}
+}
+
+int 
+main(int argc, char **argv)
+{
+	int mousefd;
+	port_t mouse;
+	struct msg m;
+
+	mousefd = open("//srv/mouse", O_RDONLY);
+	if (mousefd < 0) {
+		perror("//srv/mouse");
 		exit(1);
+	}
+	mouse = __fd_port(mousefd);
+
+	/*
+	 * Test select() support
+	 */
+	if (argc > 1) {
+		if (strcmp(argv[1], "-s")) {
+			fprintf(stderr, "Unknown flag: %s\n", argv[1]);
+			exit(1);
+		}
+		test_select(mousefd);
 	}
 
 	for (;;) {
@@ -32,12 +93,6 @@ main(void)
 		/*
 		 * Get status, and display
 		 */
-		statstr = rstat(mouse, NULL);
-		printf("%d %d %d %3d %3d\n",
-			atoi(__fieldval(statstr, "left")),
-			atoi(__fieldval(statstr, "middle")),
-			atoi(__fieldval(statstr, "right")),
-			atoi(__fieldval(statstr, "dx")),
-			atoi(__fieldval(statstr, "dy")));
+		readit(mousefd);
 	}
 }
