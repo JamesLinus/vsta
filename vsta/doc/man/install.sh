ec13a119c6d957436adb31ed7b9df88ff9e992c7
vandys <vandys>
2001-10-25 19:22:48 +0000

    New man page organization.

--- vsta/doc/man/install.sh
+++ vsta/doc/man/install.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 root=$1
-dirs="1 2 6"
+dirs="1 2 3 6"
 
 cp map $root/doc/man
 for x in $dirs
 do
+	# Create the man dir for this section
 	mkdir -p $root/doc/man/$x
-	cp $x/*.$x $root/doc/man/$x
+
+	# Make sure no stale, old man pages are present
+	rm -f $root/doc/man/$x/*.$x
+
+	# Walk our man pages, converting from nroff source
+	for y in $x/*.$x
+	do
+		nroff -man $y > $root/doc/man/$y
+	done
 done
 exit 0
